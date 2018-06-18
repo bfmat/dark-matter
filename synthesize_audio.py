758795d03e3b756cbecf448b33baf10552804357
brendon-ai <brendon-m@outlook.com>
2018-06-18 18:31:52 -0400

    Get audio file paths

--- synthesize_audio.py
+++ synthesize_audio.py
@@ -2,9 +2,20 @@
 """A program for generating realistic audio data based on existing recordings, by tweaking the overall volume as well as the frequency distribution"""
 # Created by Brendon Matusch, June 2018
 
+import os
+import sys
 import numpy as np
 from scipy.io.wavfile import read
 from verify_arguments import verify_arguments
 
-# A folder containing the real audio samples is expected
+# A folder containing the real audio samples in WAV format is expected
 verify_arguments('audio sample folder')
+
+# Get the full paths of the files in the audio sample folder
+sample_folder = os.path.expanduser(sys.argv[1])
+audio_paths = [
+    os.path.join(sample_folder, file_name)
+    for file_name in os.listdir(sample_folder)
+]
+
+# TODO: Read the audio and modify it
