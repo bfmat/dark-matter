e589ee180bb1aa3b4c185ef34ec53039351260a0
brendon-ai <brendon-m@outlook.com>
2018-06-23 12:37:04 -0400

    Revert "Started work on loading text file"
    This reverts commit 834407599c63355127658fec56b2fa16c15d83f2.

--- event_data.py
+++ event_data.py
@@ -1,8 +1,10 @@
 """Code for managing data sets that allows loading and conversion"""
 # Created by Brendon Matusch, June 2018
 
+import ROOT
+
 # The path to the main data file which contains processed attributes of bubble events
-EVENT_FILE_PATH = "/opt/merged_all_all.txt"
+EVENT_FILE_PATH = "/opt/merged_all_all.root"
 
 
 class EventDataSet:
@@ -10,8 +12,6 @@ class EventDataSet:
 
     def __init__(self) -> None:
         """Initializer that takes parameters that determine which data is loaded"""
-        # Open the event file and read its full contents
-        with open(EVENT_FILE_PATH) as event_file:
-            event_file_lines = event_file.readlines()
-        # The second line of the file contains the names of the various fields, separated by spaces
-        field_names = event_file_lines[1].split()
+        # Open the event file and get the main tree
+        tree = ROOT.TFile(EVENT_FILE_PATH).Get("T").GetEntries()
+        # TODO: Load the events
