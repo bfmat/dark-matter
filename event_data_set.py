834407599c63355127658fec56b2fa16c15d83f2
brendon-ai <brendon-m@outlook.com>
2018-06-23 12:28:13 -0400

    Started work on loading text file

--- event_data_set.py
+++ event_data_set.py
@@ -1,10 +1,8 @@
 """Code for managing data sets that allows loading and conversion"""
 # Created by Brendon Matusch, June 2018
 
-import ROOT
-
 # The path to the main data file which contains processed attributes of bubble events
-EVENT_FILE_PATH = "/opt/merged_all_all.root"
+EVENT_FILE_PATH = "/opt/merged_all_all.txt"
 
 
 class EventDataSet:
@@ -12,6 +10,8 @@ class EventDataSet:
 
     def __init__(self) -> None:
         """Initializer that takes parameters that determine which data is loaded"""
-        # Open the event file and get the main tree
-        tree = ROOT.TFile(EVENT_FILE_PATH).Get("T").GetEntries()
-        # TODO: Load the events
+        # Open the event file and read its full contents
+        with open(EVENT_FILE_PATH) as event_file:
+            event_file_lines = event_file.readlines()
+        # The second line of the file contains the names of the various fields, separated by spaces
+        field_names = event_file_lines[1].split()
