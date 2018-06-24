5b36809c652de000626cbb424d1c7fc906bc9ae5
brendon-ai <brendon-m@outlook.com>
2018-06-23 21:48:12 -0400

    Added lots of fields to the data point class

--- event_data.py
+++ event_data.py
@@ -1,6 +1,10 @@
 """Code for managing data sets that allows loading and conversion"""
 # Created by Brendon Matusch, June 2018
 
+import datetime
+import itertools
+from enum import Enum
+
 import ROOT
 
 # The path to the main data file which contains processed attributes of bubble events
@@ -16,5 +20,63 @@ class EventDataSet:
         # These cannot be in the same line or a segmentation fault will occur
         event_file = ROOT.TFile(EVENT_FILE_PATH)
         tree = event_file.Get('T')
-        # Iterate over the tree and get a list of the events
-        events = [event for event in tree]
+        # Iterate over the tree and convert the events to a custom data class
+        # For now, set the global list directly (filtering will be added)
+        self.events = [EventDataPoint(event) for event in tree]
+
+
+class EventDataPoint:
+    """A bubble event data class that contains all of the data necessary for training, for a single bubble event"""
+
+    def __init__(self, event: ROOT.TTree) -> None:
+        """Initializer that takes a single event in the CERN ROOT format and extracts the relevant data"""
+        # The run identifier is in the format YYYYMMDD_RR (R is the run within that day); parse it to get a date and a run number
+        run_identifier = event.run
+        year = int(run_identifier[:4])
+        month = int(run_identifier[4:6])
+        day = int(run_identifier[6:8])
+        self.date = datetime.date(year=year, month=month, day=day)
+        # At the end of the string, there are often strange characters; iterate up to the last one which is a numeric digit, and use that to cut out the run number
+        run_number_end_index = 10
+        for character_index in itertools.count(run_number_end_index + 1):
+            # Subtract one from the index because indexing in Python is exclusive for the end
+            if run_identifier[character_index - 1].isdigit():
+                run_number_end_index = character_index
+            else:
+                break
+        self.run_number = int(run_identifier[9:run_number_end_index])
+        # Get the event number within the run, which starts at 0
+        self.event_number = event.ev
+        # Assign the run type based on the raw numeric value, assuming garbage if the value is not present in the enumeration
+        raw_run_type = event.run_type
+        self.run_type = RunType(raw_run_type) \
+            if raw_run_type in set(possible_run_type.value for possible_run_type in RunType) \
+            else RunType.GARBAGE
+        # Likewise for the cause of the recording trigger, assuming a manual trigger or relaunch due to a problem
+        raw_trigger_cause = event.trigger_main
+        self.trigger_cause = TriggerCause(raw_trigger_cause) \
+            if raw_trigger_cause in set(possible_trigger_cause.value for possible_trigger_cause in TriggerCause) \
+            else TriggerCause.MANUAL_OR_RELAUNCH
+        # Get the position-corrected banded frequency domain representation of the audio as a list of strength values
+        self.banded_frequency_domain = list(event.piezo_E_PosCor)
+        # Get the number of bubbles present in the bubble, calculated through image matching
+        self.num_bubbles = event.nbub
+
+
+class RunType(Enum):
+    """An enumeration of the possible run types, including various distinct radiation sources; numbers correspond to the numeric representations in the data table"""
+    LOW_BACKGROUND = 0  # Run with normal background radiation
+    AMERICIUM_BERYLLIUM = 2  # Calibration with AmBe source at the end of the source tube
+    ACOUSTICS_BLINDED = 10  # Run with acoustic information blinded
+    CALIFORNIUM_40CM = 14  # Cf source at 40cm from the bottom of the source tube
+    CALIFORNIUM_60CM = 15  # Cf source at 60cm from the bottom of the source tube
+    BARIUM_100CM = 21  # Ba source at 100cm from the bottom of the source tube
+    BARIUM_40CM = 22  # Ba source at 45cm from the bottom of the source tube
+    GARBAGE = 99  # Invalid data or unknown run type
+
+
+class TriggerCause(Enum):
+    """An enumeration of the possible causes of the recording trigger"""
+    CAMERA_TRIGGER = 0  # The normal optical trigger when bubbles are observed
+    TIMEOUT = 2  # The maximum time permitted for an event was reached
+    MANUAL_OR_RELAUNCH = 3  # Either a manual trigger, or an auto-relaunch due to a problem
