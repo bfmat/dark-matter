59708e32b2a5162cfb2f6f44c1819c8a0df89e4d
brendon-ai <brendon-m@outlook.com>
2018-06-28 12:35:09 -0400

    Include only americium calibration data

--- training/image_classification_train.py
+++ training/image_classification_train.py
@@ -16,10 +16,6 @@ event_data_set = EventDataSet(
     keep_run_types=set([
         RunType.LOW_BACKGROUND,
         RunType.AMERICIUM_BERYLLIUM,
-        RunType.CALIFORNIUM_40CM,
-        RunType.CALIFORNIUM_60CM,
-        RunType.BARIUM_40CM,
-        RunType.BARIUM_100CM
     ])
 )
 # Get the bubble images and corresponding ground truths
