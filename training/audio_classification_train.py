4993e9ae1a28eba92affbe2581573d5cdcd165ff
brendon-ai <brendon-m@outlook.com>
2018-06-30 13:30:21 -0400

    Replaced architecture with fully convolutional; removed unnecessary parameter

--- training/audio_classification_train.py
+++ training/audio_classification_train.py
@@ -2,12 +2,45 @@
 """Training script for a neural network that classifies audio samples into alpha particles and neutrons"""
 # Created by Brendon Matusch, June 2018
 
-from keras.layers import Conv1D, Flatten, Dense, Dropout, InputLayer
+from keras.layers import Conv1D, Flatten, Dense, Dropout, InputLayer, BatchNormalization
 from keras.models import Sequential
 
 from data_processing.event_data_set import EventDataSet
 from data_processing.bubble_data_point import RunType, load_bubble_audio
 from data_processing.experiment_serialization import save_test
+
+# Create a one-dimensional convolutional neural network model with hyperbolic tangent activations
+# It should take both microphone channels and an entire clip of audio
+activation = 'tanh'
+model = Sequential([
+    InputLayer(input_shape=(250000, 2)),
+    BatchNormalization(),
+    Conv1D(filters=16, kernel_size=64, strides=24, activation=activation),
+    BatchNormalization(),
+    Dropout(0.25),
+    Conv1D(filters=16, kernel_size=64, strides=24, activation=activation),
+    BatchNormalization(),
+    Dropout(0.25),
+    Conv1D(filters=32, kernel_size=32, strides=12, activation=activation),
+    BatchNormalization(),
+    Dropout(0.25),
+    Conv1D(filters=64, kernel_size=8, strides=3, activation=activation),
+    BatchNormalization(),
+    Dropout(0.25),
+    Conv1D(filters=64, kernel_size=3, strides=2, activation=activation),
+    BatchNormalization(),
+    Dropout(0.25),
+    Flatten(),
+    Dense(1, activation='sigmoid')
+])
+# Output a summary of the model's architecture
+print(model.summary())
+# Use a mean squared error loss function and an Adam optimizer, and print the accuracy while training
+model.compile(
+    optimizer='adam',
+    loss='mse',
+    metrics=['accuracy']
+)
 
 # Load the event data set from the file, removing multiple-bubble events, disabling acoustic parameter cuts, and keeping background radiation and calibration runs
 event_data_set = EventDataSet(
@@ -17,7 +50,6 @@ event_data_set = EventDataSet(
         RunType.LOW_BACKGROUND,
         RunType.AMERICIUM_BERYLLIUM,
     ]),
-    filter_proportion_randomly=0.5,
     use_fiducial_cuts=False
 )
 # Create a training data generator with the audio loading function
@@ -28,33 +60,6 @@ training_generator_callable, validation_inputs, validation_ground_truths = event
     examples_replaced_per_batch=16
 )
 training_generator = training_generator_callable()
-
-# Create a one-dimensional convolutional neural network model with hyperbolic tangent activations
-# It should take both microphone channels and an entire clip of audio
-activation = 'tanh'
-model = Sequential([
-    InputLayer(input_shape=(250000, 2)),
-    Conv1D(filters=16, kernel_size=16, strides=8, activation=activation),
-    Dropout(0.25),
-    Conv1D(filters=32, kernel_size=12, strides=6, activation=activation),
-    Dropout(0.25),
-    Conv1D(filters=64, kernel_size=8, strides=4, activation=activation),
-    Dropout(0.25),
-    Flatten(),
-    Dense(64, activation=activation),
-    Dropout(0.5),
-    Dense(16, activation=activation),
-    Dropout(0.5),
-    Dense(1)
-])
-# Output a summary of the model's architecture
-print(model.summary())
-# Use a mean squared error loss function and an Adam optimizer, and print the accuracy while training
-model.compile(
-    optimizer='adam',
-    loss='mse',
-    metrics=['accuracy']
-)
 
 # Iterate over training and validation for 20 epochs
 for _ in range(20):
