e90565e8fdad0df150235193b8364aabcf73d76f
brendon-ai <brendon-m@outlook.com>
2018-07-01 11:47:51 -0400

    Normalize according to standard deviation of full distribution

--- visualization/graph_binary_predictions.py
+++ visualization/graph_binary_predictions.py
@@ -23,9 +23,11 @@ acoustic_parameters, original_neural_network_scores = zip(
 
 # Iterate over the three criteria standard deviations will be calculated for, and corresponding names
 for criterion_data, criterion_name in zip(
-    [network_outputs, original_neural_network_scores],
-    ['network outputs', 'original neural network scores']
+    [network_outputs, acoustic_parameters, original_neural_network_scores],
+    ['network outputs', 'acoustic parameters', 'original neural network scores']
 ):
+    # Divide all of these data points by the difference between the maximum and the minimum, to normalize their range
+    criterion_data /= np.std(criterion_data)
     # Iterate over both possible values of the ground truth, and corresponding names
     for ground_truth_value, ground_truth_name in zip([True, False], ['alpha particles', 'neutrons']):
         # Sort out only the data points for this criterion which correspond to ground truths of this value
@@ -34,13 +36,8 @@ for criterion_data, criterion_name in zip(
             for index in range(len(criterion_data))
             if ground_truths[index] == ground_truth_value
         ])
-        # Subtract the ideal output for this ground truth value from each of the data points
-        errors = data - int(ground_truth_value)
-        # Calculate the geometric mean of these errors, which is the standard deviation off the ideal output
-        squared_errors = errors ** 2
-        variance = np.mean(squared_errors)
-        standard_deviation = np.sqrt(variance)
-        # Output the standard deviation to the user
+        # Calculate the standard deviation of these values and output it to the user
+        standard_deviation = np.std(data)
         print(
             f'The standard deviation of {criterion_name}',
             f'for {ground_truth_name}',
@@ -54,12 +51,12 @@ point_colors = [
 ]
 # Scatter plot the acoustic parameter on the X axis and the neural network's predictions on the Y axis
 plt.scatter(
-    x=original_neural_network_scores,
+    x=acoustic_parameters,
     y=network_outputs,
     c=point_colors
 )
 # Label the X and Y axes
-plt.xlabel('Original Neural Network Score')
+plt.xlabel('Logarithmic Acoustic Parameter')
 plt.ylabel('Neural Network Prediction')
 # Display the graph on screen
 plt.show()
