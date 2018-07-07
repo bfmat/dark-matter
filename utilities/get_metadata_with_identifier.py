9e9cee1bbcd74eda735649c9c69ed84b78fe2ba6
brendon-ai <brendon-m@outlook.com>
2018-07-07 11:28:53 -0400

    Added metadata finding script

---
+++ utilities/get_metadata_with_identifier.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""A script used to get relevant metadata of a bubble, provided its unique identifier"""
+# Created by Brendon Matusch, July 2018
+
+import sys
+
+from data_processing.event_data_set import EventDataSet
+from utilities.verify_arguments import verify_arguments
+
+# A numeric unique bubble identifier is expected
+verify_arguments('unique bubble identifier')
+
+# Load that one bubble from the data file
+identifier = int(sys.argv[1])
+data_set = EventDataSet.load_specific_indices([identifier])
+bubble = data_set.validation_events[0]
+
+# Print the date, run number, event number, and run type
+print('Date:', bubble.date)
+print('Run number:', bubble.run_number)
+print('Event number:', bubble.event_number)
+print('Run type:', bubble.run_type)
