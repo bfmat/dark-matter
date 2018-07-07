a69d10ece42ddb85e7e5768d3483399cceafad71
brendon-ai <brendon-m@outlook.com>
2018-07-07 15:43:53 -0400

    Added L2 regularization to very deep network

--- models/very_deep_convolutional_network.py
+++ models/very_deep_convolutional_network.py
@@ -3,6 +3,7 @@
 
 from keras.layers import Conv1D, MaxPooling1D, Flatten, Dropout, InputLayer, BatchNormalization, Dense
 from keras.models import Model, Sequential
+from keras.regularizers import l2
 
 
 def create_model() -> Model:
@@ -10,33 +11,59 @@ def create_model() -> Model:
     # Create a one-dimensional convolutional neural network model with rectified linear activations
     # It should take both microphone channels and an entire clip of audio
     activation = 'relu'
+    regularizer = l2(0.01)
     model = Sequential()
     model.add(InputLayer(input_shape=(250000, 2)))
     model.add(BatchNormalization())
-    model.add(Conv1D(filters=48, kernel_size=80,
-                     strides=4, activation=activation))
+    model.add(Conv1D(
+        filters=48,
+        kernel_size=80,
+        strides=4,
+        activation=activation,
+        kernel_regularizer=regularizer
+    ))
     model.add(MaxPooling1D(8))
     model.add(BatchNormalization())
     for _ in range(3):
-        model.add(Conv1D(filters=48, kernel_size=3, activation=activation))
+        model.add(Conv1D(
+            filters=48,
+            kernel_size=3,
+            activation=activation,
+            kernel_regularizer=regularizer
+        ))
         model.add(BatchNormalization())
     model.add(MaxPooling1D(8))
     model.add(BatchNormalization())
     for _ in range(4):
-        model.add(Conv1D(filters=96, kernel_size=3, activation=activation))
+        model.add(Conv1D(
+            filters=96,
+            kernel_size=3,
+            activation=activation,
+            kernel_regularizer=regularizer
+        ))
         model.add(BatchNormalization())
     model.add(MaxPooling1D(8))
     model.add(BatchNormalization())
     for _ in range(6):
-        model.add(Conv1D(filters=192, kernel_size=3, activation=activation))
+        model.add(Conv1D(
+            filters=192,
+            kernel_size=3,
+            activation=activation,
+            kernel_regularizer=regularizer
+        ))
         model.add(BatchNormalization())
     model.add(MaxPooling1D(8))
     model.add(BatchNormalization())
     for _ in range(3):
-        model.add(Conv1D(filters=384, kernel_size=3, activation=activation))
+        model.add(Conv1D(
+            filters=384,
+            kernel_size=3,
+            activation=activation,
+            kernel_regularizer=regularizer
+        ))
         model.add(BatchNormalization())
     model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(1, activation='sigmoid', kernel_regularizer=regularizer))
     # Output a summary of the model's architecture
     print(model.summary())
     # Use a mean squared error loss function and an Adam optimizer, and print the accuracy while training
