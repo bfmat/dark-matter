7c5b666e8224c42915df1dc44436fab3018e7b60
brendon-ai <brendon-m@outlook.com>
2018-07-09 13:25:31 -0400

    Cut the beginning and end of the audio

--- data_processing/bubble_data_point.py
+++ data_processing/bubble_data_point.py
@@ -182,6 +182,8 @@ def load_bubble_audio(bubble: Optional[BubbleDataPoint], audio_file_path: Option
     data_array = np.reshape(data_array_flat, (samples, CHANNELS))
     # Index and return the data of microphones 3 and 7, the only ones that work
     data_array = data_array[:, [0, 3]]
+    # Cut out most of the empty noise at the beginning, and the hydraulic pump sounds at the end
+    data_array = data_array[90_000:190_000]
     # Convert the array to 64-bit floats to prevent overflows
     data_array = data_array.astype(np.float64)
     # Normalize the audio array so its geometric mean is 1
