73057cd7e12302329c086385a523cd9f3d54e4ba
brendon-ai <brendon-m@outlook.com>
2018-07-09 12:12:58 -0400

    Offer time-banded frequency domain noise

--- data_processing/audio_synthesis.py
+++ data_processing/audio_synthesis.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-from data_processing.audio_domain_processing import time_to_frequency_domain, frequency_to_time_domain
+from data_processing.audio_domain_processing import time_to_frequency_domain, frequency_to_time_domain, band_time_domain
 
 
 def normalize(audio: np.ndarray) -> np.ndarray:
@@ -27,17 +27,26 @@ def add_time_noise(audio: np.ndarray, standard_deviation: float) -> np.ndarray:
     return audio + noise
 
 
-def multiply_frequency_noise(audio: np.ndarray, standard_deviation: float) -> np.ndarray:
+def multiply_frequency_noise(audio: np.ndarray, standard_deviation: float, time_bands: int = 1) -> np.ndarray:
     """Incorporate multiplicative Gaussian noise into an audio recording, such that frequency domain values are modified randomly proportionately to their absolute magnitudes"""
-    # Convert the audio into the frequency domain
-    audio_frequency = time_to_frequency_domain(audio)
-    # Create a noise array of the same size, centered at 1 (because it is multiplicative)
-    noise = np.random.normal(
-        loc=1,
-        scale=standard_deviation,
-        size=audio_frequency.shape
-    )
-    # Element-wise multiply the audio array by the noise
-    audio_frequency *= noise
-    # Return the modified audio, converted back to the time domain
-    return frequency_to_time_domain(audio_frequency)
+    # Split the audio into a number of time bands (a larger number of bands means more localized application of frequency domain noise)
+    audio_bands = band_time_domain(audio, time_bands)
+    # Create a list to add modified bands to, and iterate over the original bands
+    modified_audio_bands = []
+    for audio_band in audio_bands:
+        # Convert the audio band into the frequency domain
+        audio_frequency = time_to_frequency_domain(audio_band)
+        # Create a noise array of the same size, centered at 1 (because it is multiplicative)
+        noise = np.random.normal(
+            loc=1,
+            scale=standard_deviation,
+            size=audio_frequency.shape
+        )
+        # Element-wise multiply the audio array by the noise
+        audio_frequency *= noise
+        # Convert the modified band back into the time domain
+        modified_band = frequency_to_time_domain(audio_frequency)
+        # Add the modified band to the list
+        modified_audio_bands.append(modified_band)
+    # Return the modified bands, concatenated together along the samples axis
+    return np.concatenate(modified_audio_bands, axis=0)
