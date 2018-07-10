581cafb445104a7ada12f664f465b3a8c4d19073
brendon-ai <brendon-m@outlook.com>
2018-07-10 09:43:58 -0400

    Include fiducial cuts for validation

--- training/audio_classification_train.py
+++ training/audio_classification_train.py
@@ -10,8 +10,8 @@ from data_processing.bubble_data_point import RunType, load_bubble_audio
 from data_processing.experiment_serialization import save_test
 from models.very_deep_convolutional_network import create_model
 
-# Load the event data set from the file, removing multiple-bubble events, disabling acoustic parameter cuts, and keeping background radiation and calibration runs
-event_data_set = EventDataSet(
+# Load two different data sets from the file, one for training which does not include fiducial cuts, and one for validation which does
+training_data_set = EventDataSet(
     filter_multiple_bubbles=True,
     keep_run_types=set([
         RunType.LOW_BACKGROUND,
@@ -23,12 +23,24 @@ event_data_set = EventDataSet(
     ]),
     use_fiducial_cuts=False
 )
+validation_data_set = EventDataSet(
+    filter_multiple_bubbles=True,
+    keep_run_types=set([
+        RunType.LOW_BACKGROUND,
+        RunType.AMERICIUM_BERYLLIUM,
+        RunType.CALIFORNIUM_40CM,
+        RunType.CALIFORNIUM_60CM,
+        RunType.BARIUM_100CM,
+        RunType.BARIUM_40CM
+    ]),
+    use_fiducial_cuts=True
+)
 # If the option "wall" is passed, discriminate between wall and non-wall events; otherwise, use the default
 ground_truth = EventDataSet.is_not_wall_event \
     if len(sys.argv) >= 2 and sys.argv[1].lower() == "wall" \
     else None
 # Create a training data generator with the audio loading function and whatever ground truth function has been chosen
-training_generator_callable, validation_inputs, validation_ground_truths = event_data_set.arbitrary_alpha_classification_generator(
+training_generator_callable, _, _ = training_data_set.arbitrary_alpha_classification_generator(
     data_converter=load_bubble_audio,
     storage_size=512,
     batch_size=32,
@@ -36,6 +48,14 @@ training_generator_callable, validation_inputs, validation_ground_truths = event
     ground_truth=ground_truth
 )
 training_generator = training_generator_callable()
+# Get validation data from the set which has fiducial cuts enabled (since this is the data for which inference accuracy is very meaningful)
+_, validation_inputs, validation_ground_truths = validation_data_set.arbitrary_alpha_classification_generator(
+    data_converter=load_bubble_audio,
+    storage_size=512,
+    batch_size=32,
+    examples_replaced_per_batch=16,
+    ground_truth=ground_truth
+)
 # Create an instance of the fully convolutional network model
 model = create_model()
 # Iterate over training and validation for 20 epochs
@@ -58,7 +78,7 @@ for epoch in range(20):
     # Run predictions on the validation data set, and save the experimental run
     validation_network_outputs = model.predict(validation_inputs)
     save_test(
-        event_data_set,
+        validation_data_set,
         validation_ground_truths,
         validation_network_outputs,
         epoch,
