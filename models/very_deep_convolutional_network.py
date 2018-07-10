431c02f4dff3d6f0d8ad85e711835498918220b5
brendon-ai <brendon-m@outlook.com>
2018-07-09 20:39:31 -0400

    Use functional API for network model

--- models/very_deep_convolutional_network.py
+++ models/very_deep_convolutional_network.py
@@ -1,69 +1,75 @@
 """A very deep 1-dimensional fully convolutional network intended for processing of raw audio waveforms and inspired by the M34 architecture"""
 # Created by Brendon Matusch, July 2018
 
-from keras.layers import Conv1D, MaxPooling1D, Flatten, Dropout, InputLayer, BatchNormalization, Dense
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Dropout, Input, BatchNormalization, Dense
 from keras.models import Model, Sequential
 from keras.regularizers import l2
 
 
 def create_model() -> Model:
     """Create and return a new instance of the very deep convolutional network"""
-    # Create a one-dimensional convolutional neural network model with rectified linear activations
+    # Create a one-dimensional convolutional neural network model with rectified linear activations, using the Keras functional API
     # It should take both microphone channels and an entire clip of audio
     activation = 'relu'
+    padding = 'same'
     regularizer = l2(0)
-    model = Sequential()
-    model.add(InputLayer(input_shape=(100_000, 2)))
-    model.add(BatchNormalization())
-    model.add(Conv1D(
+    inputs = Input((100_000, 2))
+    x = BatchNormalization()(inputs)
+    x = Conv1D(
         filters=48,
         kernel_size=80,
         strides=4,
         activation=activation,
-        kernel_regularizer=regularizer
-    ))
-    model.add(MaxPooling1D(6))
-    model.add(BatchNormalization())
+        kernel_regularizer=regularizer,
+        padding=padding
+    )(x)
+    x = MaxPooling1D(6)(x)
+    x = BatchNormalization()(x)
     for _ in range(3):
-        model.add(Conv1D(
+        x = Conv1D(
             filters=48,
             kernel_size=3,
             activation=activation,
-            kernel_regularizer=regularizer
-        ))
-        model.add(BatchNormalization())
-    model.add(MaxPooling1D(6))
-    model.add(BatchNormalization())
+            kernel_regularizer=regularizer,
+            padding=padding
+        )(x)
+        x = BatchNormalization()(x)
+    x = MaxPooling1D(6)(x)
+    x = BatchNormalization()(x)
     for _ in range(4):
-        model.add(Conv1D(
+        x = Conv1D(
             filters=96,
             kernel_size=3,
             activation=activation,
-            kernel_regularizer=regularizer
-        ))
-        model.add(BatchNormalization())
-    model.add(MaxPooling1D(6))
-    model.add(BatchNormalization())
+            kernel_regularizer=regularizer,
+            padding=padding
+        )(x)
+        x = BatchNormalization()(x)
+    x = MaxPooling1D(6)(x)
+    x = BatchNormalization()(x)
     for _ in range(6):
-        model.add(Conv1D(
+        x = Conv1D(
             filters=192,
             kernel_size=3,
             activation=activation,
-            kernel_regularizer=regularizer
-        ))
-        model.add(BatchNormalization())
-    model.add(MaxPooling1D(6))
-    model.add(BatchNormalization())
+            kernel_regularizer=regularizer,
+            padding=padding
+        )(x)
+        x = BatchNormalization()(x)
+    x = MaxPooling1D(6)(x)
+    x = BatchNormalization()(x)
     for _ in range(3):
-        model.add(Conv1D(
+        x = Conv1D(
             filters=384,
             kernel_size=3,
             activation=activation,
-            kernel_regularizer=regularizer
-        ))
-        model.add(BatchNormalization())
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid', kernel_regularizer=regularizer))
+            kernel_regularizer=regularizer,
+            padding=padding
+        )(x)
+        x = BatchNormalization()(x)
+    x = Flatten()(x)
+    outputs = Dense(1, activation='sigmoid', kernel_regularizer=regularizer)(x)
+    model = Model(inputs=inputs, outputs=outputs)
     # Output a summary of the model's architecture
     print(model.summary())
     # Use a mean squared error loss function and an Adam optimizer, and print the accuracy while training
