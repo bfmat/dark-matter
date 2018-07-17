714c9231f5a58e7cdbc29a13db6814aba7de44b0
brendon-ai <brendon-m@outlook.com>
2018-07-17 07:16:34 -0400

    Save experimental data and trained model every epoch

--- training/banded_frequency_train.py
+++ training/banded_frequency_train.py
@@ -2,6 +2,8 @@
 """Training script for a neural network that is trained on a recording in frequency domain split into discrete bands"""
 # Created by Brendon Matusch, June 2018
 
+import os
+
 from keras.layers import Dense, Dropout, BatchNormalization, InputLayer
 from keras.models import Sequential
 
@@ -40,19 +42,26 @@ model.compile(
     metrics=['accuracy']
 )
 
-# Train the model on the loaded data set
-model.fit(
-    x=training_input,
-    y=training_ground_truths,
-    validation_data=(validation_input, validation_ground_truths),
-    epochs=EPOCHS
-)
-# Run predictions on the validation data set, and save the experimental run
-validation_network_outputs = model.predict(validation_input)
-save_test(
-    event_data_set,
-    validation_ground_truths,
-    validation_network_outputs,
-    epoch=EPOCHS - 1,
-    prefix='banded_low_resolution_'
-)
+
+# Iterate over the defined number of epochs
+for epoch in range(EPOCHS):
+    # Train the model on the loaded data set
+    model.fit(
+        x=training_input,
+        y=training_ground_truths,
+        validation_data=(validation_input, validation_ground_truths),
+        epochs=1,
+        verbose=False
+    )
+    # Run predictions on the validation data set, and save the experimental run
+    validation_network_outputs = model.predict(validation_input)
+    save_test(
+        event_data_set,
+        validation_ground_truths,
+        validation_network_outputs,
+        epoch=epoch,
+        prefix='banded_low_resolution_'
+    )
+    # Save the current model, named with the epoch number
+    model_path = os.path.expanduser(f'~/banded_epoch{epoch}.h5')
+    model.save(model_path)
