3deda16908895576e2ceb226deabd0548c538475
brendon-ai <brendon-m@outlook.com>
2018-07-18 10:38:09 -0400

    Iterative cluster nucleation is functional

--- training/iterative_cluster_nucleation_train.py
+++ training/iterative_cluster_nucleation_train.py
@@ -3,90 +3,71 @@
 # Created by Brendon Matusch, July 2018
 
 import copy
-from typing import Callable, List
 
 import numpy as np
 
-from data_processing.bubble_data_point import BubbleDataPoint, load_bubble_audio, RunType
+from data_processing.bubble_data_point import RunType
 from data_processing.event_data_set import EventDataSet
-from models.very_deep_convolutional_network import create_model
+from data_processing.experiment_serialization import save_test
+from models.banded_frequency_network import create_model
+
+# The initial number of examples to put in the training set
+INITIAL_TRAINING_EXAMPLES = 128
 
 # The distance from 0 or 1 an example must be to be added to the training set
 TRAINING_THRESHOLD_DISTANCE = 0.025
 
-
-def confident_enough_for_initial_set(bubble: BubbleDataPoint) -> bool:
-    """A function that, given a bubble data point, returns whether or not its classification is sufficiently confident for the initial training data set"""
-    # Run different checks depending on whether the bubble is expected to be an alpha or not
-    if bubble.run_type == RunType.LOW_BACKGROUND:
-        # Accept only alphas with an acoustic parameter above a certain threshold
-        if bubble.logarithmic_acoustic_parameter < 0.9:
-            return False
-    else:
-        # Accept only neutrons with an acoustic parameter below a certain threshold
-        if bubble.logarithmic_acoustic_parameter > 1.5:
-            return False
-    # If none of the checks fail, the bubble's classification is sufficiently confident
-    return True
-
-
 # Create a data set, running fiducial cuts for the most reasonable data
-event_data_set = EventDataSet(
-    filter_multiple_bubbles=True,
-    keep_run_types=set([
-        RunType.LOW_BACKGROUND,
-        RunType.AMERICIUM_BERYLLIUM,
-        RunType.CALIFORNIUM_40CM,
-        RunType.CALIFORNIUM_60CM,
-        RunType.BARIUM_100CM,
-        RunType.BARIUM_40CM
-    ]),
-    use_fiducial_cuts=True
-)
-# Combine the training and validation sets into one
-bubbles = event_data_set.training_events + event_data_set.validation_events
-# Create a list for the gradually expanding set of training bubbles, and add the confident bubbles to it initially
-training_bubbles = [
-    bubble for bubble in bubbles
-    if confident_enough_for_initial_set(bubble)
-]
+event_data_set = EventDataSet({
+    RunType.LOW_BACKGROUND,
+    RunType.AMERICIUM_BERYLLIUM,
+    RunType.CALIFORNIUM
+})
+# Make a copy of the full training set, and then truncate the actual training set to a predefined length
+original_training_events = event_data_set.training_events.copy()
+event_data_set.training_events = event_data_set.training_events[:INITIAL_TRAINING_EXAMPLES]
 # Create an instance of the fully convolutional network model
 model = create_model()
 # Run several training iterations, each containing a number of epochs
 for iteration in range(20):
     # Output the number of examples there are in the training set for this epoch
-    print(len(training_bubbles), 'training examples for iteration', iteration)
-    # Create a training data generator with the current list of bubbles
-    training_generator_callable, _, _ = event_data_set.arbitrary_alpha_classification_generator(
-        data_converter=load_bubble_audio,
-        storage_size=256,
-        batch_size=32,
-        examples_replaced_per_batch=16,
-        custom_training_data=training_bubbles
-    )
-    training_generator = training_generator_callable()
+    print(len(event_data_set.training_events),
+          'training examples for iteration', iteration)
+    # Load the training and validation data with the current examples
+    training_input, training_ground_truths, validation_input, validation_ground_truths = event_data_set.banded_frequency_alpha_classification()
     # Train the model for a certain number of epochs on the generator
-    model.fit_generator(
-        training_generator,
-        steps_per_epoch=128,
+    model.fit(
+        x=training_input,
+        y=training_ground_truths,
+        validation_data=(validation_input, validation_ground_truths),
         epochs=10
     )
-    # Iterate over the entire list of bubbles, running predictions
-    for bubble in bubbles:
-        # Load the audio samples for that bubble (there will either be 0 or 1)
-        audio_samples = load_bubble_audio(bubble)
-        # If there are no audio samples, skip to the next iteration
-        if not audio_samples:
-            continue
-        # Add a batch dimension with one element to the NumPy array
-        audio_sample = np.expand_dims(audio_samples[0], axis=0)
+    # Run predictions on the validation data set, and save the experimental run
+    validation_network_outputs = model.predict(validation_input)
+    save_test(
+        event_data_set,
+        validation_ground_truths,
+        validation_network_outputs,
+        epoch=iteration,
+        prefix='iterative_cluster_nucleation_'
+    )
+    # Iterate over the entire list of potential training examples, running predictions
+    for event in original_training_events:
+        # Get the frequency domain input data from the event, and add a batch axis
+        input_data = np.expand_dims(
+            np.concatenate([
+                event.banded_frequency_domain_raw[1:].flatten(),
+                [event.x_position, event.y_position, event.z_position]
+            ]),
+            axis=0
+        )
         # Run a prediction on the audio sample using the existing neural network
-        prediction = model.predict(audio_sample)
+        prediction = model.predict(input_data)
         # If the prediction is within a certain threshold distance of either 0 or 1
         if min([prediction, 1 - prediction]) < TRAINING_THRESHOLD_DISTANCE:
-            # Copy the bubble and set its run type so that it is in the corresponding ground truth
-            bubble_copy = copy.deepcopy(bubble)
+            # Copy the event and set its run type so that it is in the corresponding ground truth
+            bubble_copy = copy.deepcopy(event)
             bubble_copy.run_type = RunType.AMERICIUM_BERYLLIUM if bool(round(prediction[0, 0])) \
                 else RunType.LOW_BACKGROUND
             # Add the modified bubble to the training list
-            training_bubbles.append(bubble_copy)
+            event_data_set.training_events.append(bubble_copy)
