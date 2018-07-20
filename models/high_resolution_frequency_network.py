de1a69266d44af03dd64ad173c0f715cb9c195c5
brendon-ai <brendon-m@outlook.com>
2018-07-20 14:08:34 -0400

    Added L2 regularization to hopefully reduce overfitting

--- models/high_resolution_frequency_network.py
+++ models/high_resolution_frequency_network.py
@@ -3,22 +3,24 @@
 
 from keras.layers import Dense, Dropout, BatchNormalization, Input, concatenate
 from keras.models import Model
+from keras.regularizers import l2
 
 
 def create_model() -> Model:
     """Create and return a new instance of the high-resolution frequency network"""
-    # Create a neural network composed of dense layers with dropout regularization, using hyperbolic tangent activations
+    # Create a neural network composed of dense layers with dropout and L2 regularization, using hyperbolic tangent activations
     activation = 'tanh'
+    regularizer = l2(0.003)
     # Create two inputs, one for the audio data and one for the position, and concatenate them together
     audio_input = Input((80,))
     position_input = Input((3,))
     x = concatenate([audio_input, position_input])
     x = BatchNormalization()(x)
-    x = Dense(12, activation=activation)(x)
+    x = Dense(12, activation=activation, kernel_regularizer=regularizer)(x)
     x = Dropout(0.5)(x)
-    x = Dense(8, activation=activation)(x)
+    x = Dense(8, activation=activation, kernel_regularizer=regularizer)(x)
     x = Dropout(0.5)(x)
-    output = Dense(1, activation='sigmoid')(x)
+    output = Dense(1, activation='sigmoid', kernel_regularizer=regularizer)(x)
     # Create a model with both inputs
     model = Model(inputs=[audio_input, position_input], outputs=output)
     # Output a summary of the model's architecture
