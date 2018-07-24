e2f2039e176b97a31de0953d0649a291ecd51621
brendon-ai <brendon-m@outlook.com>
2018-07-24 16:46:34 -0400

    Added a bit more to statistics script

--- statistics/grid_search_statistics.py
+++ statistics/grid_search_statistics.py
@@ -11,3 +11,14 @@ from utilities.verify_arguments import verify_arguments
 verify_arguments('log file path')
 # Get it from the command line arguments
 log_path = os.path.expanduser(sys.argv[1])
+# Read the full contents of the file into memory
+with open(log_path) as log_file:
+    log_data = log_file.read()
+# Split the test into different tests based on the part where the hyperparameters are printed
+# Remove data before the first test, which consists of system information printed at the beginning
+tests = log_data.split('HYPERPARAMETERS')[1:]
+# Iterate over the tests, printing statistics for each one
+for test in tests:
+    # Split the string into its component lines
+    test_lines = test.split(os.linesep)
+    print(test_lines)
