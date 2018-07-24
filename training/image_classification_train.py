f923a02e76d58f7bcfbb0fa194f4a3568c1bb6cd
brendon-ai <brendon-m@outlook.com>
2018-07-24 14:16:41 -0400

    Removed horrendous generator stuff, now loading images in a sane way

--- training/image_classification_train.py
+++ training/image_classification_train.py
@@ -2,8 +2,11 @@
 """Training script for a neural network that classifies images of bubbles into alpha particles and neutrons"""
 # Created by Brendon Matusch, June 2018
 
+import os
+
 from data_processing.event_data_set import EventDataSet, RunType
 from data_processing.bubble_data_point import load_bubble_images
+from data_processing.experiment_serialization import save_test
 from models.image_classification_network import create_model
 
 # Load the event data set from the file, removing multiple-bubble events, disabling acoustic parameter cuts, and keeping background radiation and calibration runs
@@ -11,24 +14,22 @@ event_data_set = EventDataSet({
     RunType.LOW_BACKGROUND,
     RunType.AMERICIUM_BERYLLIUM,
 })
-# Create a training data generator and get validation data array with the image loading function
-training_generator_callable, validation_inputs, validation_ground_truths = event_data_set.arbitrary_alpha_classification_generator(
-    data_converter=load_bubble_images,
-    storage_size=512,
-    batch_size=32,
-    examples_replaced_per_batch=16
-)
-training_generator = training_generator_callable()
+# Load the image data as NumPy arrays
+training_inputs, training_ground_truths, validation_inputs, validation_ground_truths = \
+    event_data_set.audio_alpha_classification(
+        loading_function=load_bubble_images,
+        include_positions=False
+    )
 
 # Create an instance of the image processing convolutional neural network
 model = create_model()
 
-# Iterate over training and validation for 20 epochs
-for _ in range(20):
-    # Train the model on the generator
-    model.fit_generator(
-        training_generator,
-        steps_per_epoch=128,
+# Iterate over training and validation for several epochs
+for epoch in range(20):
+    # Train the model on the input and ground truth arrays
+    model.fit(
+        x=training_inputs,
+        y=training_ground_truths,
         epochs=1
     )
     # Evaluate the model on the validation data set
@@ -37,5 +38,18 @@ for _ in range(20):
         y=validation_ground_truths,
         verbose=0
     )
+    # Output the validation loss and accuracy to the user
     print('Validation loss:', loss)
     print('Validation accuracy:', accuracy)
+    # Run predictions on the validation data set, and save the experimental run
+    validation_network_outputs = model.predict(validation_inputs)
+    save_test(
+        event_data_set,
+        validation_ground_truths,
+        validation_network_outputs,
+        epoch=epoch,
+        prefix='image_'
+    )
+    # Save the current model, named with the epoch number
+    model_path = os.path.expanduser(f'~/image_epoch{epoch}.h5')
+    model.save(model_path)
