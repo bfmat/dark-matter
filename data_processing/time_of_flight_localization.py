6c1e80b52844d897a87998a4208b0426a40bdc9f
brendon-ai <brendon-m@outlook.com>
2018-07-24 08:30:34 -0400

    Optimization works!

--- data_processing/time_of_flight_localization.py
+++ data_processing/time_of_flight_localization.py
@@ -2,6 +2,8 @@
 # Created by Brendon Matusch, July 2018
 
 import autograd.numpy as np
+from autograd import grad
+from scipy.optimize import minimize
 
 # A list of the positions of the piezos in 3D space, using the same units as the bubble positions in the ROOT file
 PIEZO_POSITIONS = [
@@ -17,16 +19,16 @@ SPEED_OF_SOUND = 1
 
 def expected_times_of_flight(bubble_position):
     """Calculate the expected times of flight, from a bubble at a certain point to each of the piezos"""
-    # Create an array to hold the expected times of flight
-    times_of_flight = np.zeros(len(PIEZO_POSITIONS))
-    # Iterate over each of the piezo positions, with a corresponding index
-    for index, piezo_position in enumerate(PIEZO_POSITIONS):
+    # Create a list to hold the expected times of flight
+    times_of_flight = []
+    # Iterate over each of the piezo positions
+    for piezo_position in PIEZO_POSITIONS:
         # Calculate the distance between this piezo and the bubble
         distance = np.linalg.norm(bubble_position - piezo_position)
-        # Divide the distance by the speed of sound and add it to the array
-        times_of_flight[index] = distance / SPEED_OF_SOUND
-    # Return the array containing the times of flight for every piezo
-    return times_of_flight
+        # Divide the distance by the speed of sound and add it to the list
+        times_of_flight.append(distance / SPEED_OF_SOUND)
+    # Return the list of times of flight as an Autograd NumPy array
+    return np.array(times_of_flight)
 
 
 def timing_error(bubble_position, piezo_timings):
@@ -37,3 +39,19 @@ def timing_error(bubble_position, piezo_timings):
     expected_timings = times_of_flight - np.min(times_of_flight)
     # Return the mean squared error between the actually observed piezo timings and the timings that would be expected for this bubble
     return np.linalg.norm(expected_timings - piezo_timings)
+
+
+def localize_bubble(piezo_timings):
+    """Calculate the position of a bubble, based on the timings of the signals to the piezos (of which the lowest value is expected to be 0), using error optimization"""
+    # Convert the piezo timings to an Autograd NumPy array
+    piezo_timings_numpy = np.array(piezo_timings)
+    # Get the derivative of the timing error calculation function, used for optimization
+    timing_error_derivative = grad(timing_error)
+    # Optimize and return the position of the bubble, starting the search at (0, 0, 0)
+    return minimize(
+        fun=timing_error,
+        x0=np.zeros(3),
+        args=(piezo_timings_numpy,),
+        method='BFGS',
+        jac=timing_error_derivative
+    ).x
