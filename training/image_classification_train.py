27287282a7087ebd0186b712c88f328c74d5dab1
brendon-ai <brendon-m@outlook.com>
2018-07-24 13:48:18 -0400

    Fixed data set constructor

--- training/image_classification_train.py
+++ training/image_classification_train.py
@@ -7,16 +7,10 @@ from data_processing.bubble_data_point import load_bubble_images
 from models.image_classification_network import create_model
 
 # Load the event data set from the file, removing multiple-bubble events, disabling acoustic parameter cuts, and keeping background radiation and calibration runs
-event_data_set = EventDataSet(
-    filter_multiple_bubbles=True,
-    filter_acoustic_parameter=False,
-    keep_run_types=set([
-        RunType.LOW_BACKGROUND,
-        RunType.AMERICIUM_BERYLLIUM,
-    ]),
-    filter_proportion_randomly=0,
-    use_fiducial_cuts=True
-)
+event_data_set = EventDataSet({
+    RunType.LOW_BACKGROUND,
+    RunType.AMERICIUM_BERYLLIUM,
+})
 # Create a training data generator and get validation data array with the image loading function
 training_generator_callable, validation_inputs, validation_ground_truths = event_data_set.arbitrary_alpha_classification_generator(
     data_converter=load_bubble_images,
