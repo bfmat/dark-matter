2760cda5903dc75b6a904f4ce32224fbcdac18bb
brendon-ai <brendon-m@outlook.com>
2018-07-25 13:41:10 -0400

    Added configuration for PICO-60 run 1

--- data_processing/event_data_set.py
+++ data_processing/event_data_set.py
@@ -11,8 +11,13 @@ import numpy as np
 from data_processing.bubble_data_point import BubbleDataPoint, RunType, TriggerCause, load_bubble_audio
 
 
-# The path to the Pickle data file which contains processed attributes of bubble events
-EVENT_FILE_PATH = os.path.expanduser('~/merged.pkl')
+# The paths to the Pickle data files, which contains processed attributes of bubble events, for each of the two PICO-60 runs
+RUN_1_PATH = os.path.expanduser('~/run1merged.pkl')
+RUN_2_PATH = os.path.expanduser('~/run2merged.pkl')
+# A constant that defines which run we are currently using
+USE_RUN_1 = True
+# Set the path to use accordingly
+EVENT_FILE_PATH = RUN_1_PATH if USE_RUN_1 else RUN_2_PATH
 
 # The number of examples to include in the validation set
 VALIDATION_EXAMPLES = 128
