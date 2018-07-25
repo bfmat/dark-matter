cec606da450ec50742cf19208bfd0885776728a6
brendon-ai <brendon-m@outlook.com>
2018-07-25 08:40:00 -0400

    Print mean and maximum training and validation accuracy values

--- statistics/grid_search_statistics.py
+++ statistics/grid_search_statistics.py
@@ -17,8 +17,29 @@ with open(log_path) as log_file:
 # Split the test into different tests based on the part where the hyperparameters are printed
 # Remove data before the first test, which consists of system information printed at the beginning
 tests = log_data.split('HYPERPARAMETERS')[1:]
-# Iterate over the tests, printing statistics for each one
-for test in tests:
+# Iterate over the tests with corresponding indices, printing statistics for each one
+for test_index, test in enumerate(tests):
+    # Print a blank line for separation
+    print()
+    # Print out the index of this test
+    print('Test', test_index)
     # Split the string into its component lines
     test_lines = test.split(os.linesep)
-    print(test_lines)
+    # Strip whitespace from the lines, and remove all blank lines after this
+    test_lines = [line.strip() for line in test_lines]
+    test_lines = [line for line in test_lines if line]
+    # Print out the hyperparameter description lines until the separator line that starts with an underscore
+    hyperparameter_lines = []
+    for line in test_lines:
+        if line.startswith('_'):
+            break
+        print(line)
+    # Get the training accuracy out of lines containing it; lines at the end of the epoch include the word 'step'
+    training_accuracy = [float(line.split('acc:')[1].strip()) for line in test_lines if 'step' in line]
+    # Get the validation accuracy out of lines containing it
+    validation_accuracy = [float(line.split()[2]) for line in test_lines if 'Validation accuracy:' in line]
+    # Print the mean and maximum training and validation accuracy values
+    print('Mean training accuracy:', sum(training_accuracy) / len(training_accuracy))
+    print('Maximum training accuracy:', max(training_accuracy))
+    print('Mean validation accuracy:', sum(validation_accuracy) / len(validation_accuracy))
+    print('Maximum validation accuracy:', max(validation_accuracy))
