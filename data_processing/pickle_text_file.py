545ce5c96b1cb69dff07c8a0257f1b8b5dd5864c
brendon-ai <brendon-m@outlook.com>
2018-07-25 14:35:34 -0400

    Correspond format strings to attribute types

--- data_processing/pickle_text_file.py
+++ data_processing/pickle_text_file.py
@@ -8,20 +8,28 @@ from functools import reduce
 
 from data_processing.event_data_set import EVENT_FILE_PATH
 
+# A dictionary corresponding the last letter of a format string to the type of that element
+TYPES_BY_FORMAT = {
+    'd': int,
+    'e': float,
+    'f': float,
+    's': str
+}
+
 # Open the text file and read it line by line
 with open(os.path.expanduser('~/merged_all.txt')) as text_file:
     # Read and ignore the first line, which contains useless description
     text_file.readline()
     # Read the second line, strip it, and split it on whitespace; it includes the names and dimensions of attributes in the file
     attributes = text_file.readline().strip().split()
-    # Iterate over the attributes, adding to lists of names and corresponding numbers of sub-elements, where 0 represents a single element
+    # Iterate over the attributes, adding to lists of names and corresponding numbers of sub-elements
     attribute_names = []
     attribute_elements = []
     for attribute in attributes:
-        # If there are no brackets containing numbers of elements, add the name and set the number of elements to 0
+        # If there are no brackets containing numbers of elements, add the name and set the number of elements to 1
         if '(' not in attribute:
             attribute_names.append(attribute)
-            attribute_elements.append(0)
+            attribute_elements.append(1)
         # Otherwise, we need to handle a multi-element attribute
         else:
             # Split the attribute description by the open bracket and extract the name, which is before the brackets
@@ -31,5 +39,9 @@ with open(os.path.expanduser('~/merged_all.txt')) as text_file:
             dimensions = [int(number_string.strip()) for number_string in attribute_split[1][:-1].split(',')]
             # Multiply each of the dimensions together to get the total number of elements in this attribute, and add it to the list
             elements = reduce(lambda first, second: first * second, dimensions)
-            print(elements)
             attribute_elements.append(elements)
+    # Read and split the next line, which contains format strings for every element in the lines ahead
+    format_strings = text_file.readline().strip().split()
+    # Take only the last letter of each format string, which denotes the type, and get the types corresponding to these from the dictionary
+    attribute_types = [TYPES_BY_FORMAT[format_string[-1]] for format_string in format_strings]
+    print(attribute_types)
