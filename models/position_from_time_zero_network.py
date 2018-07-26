38ea070ed077bd095d885493ec75b92185c291ef
brendon-ai <brendon-m@outlook.com>
2018-07-26 16:16:27 -0400

    Completely removed regularization, just in case

--- models/position_from_time_zero_network.py
+++ models/position_from_time_zero_network.py
@@ -1,27 +1,21 @@
 """A fully connected neural network architecture intended to process time zero information and predict positions"""
 # Created by Brendon Matusch, July 2018
 
-from keras.layers import Dense, Dropout, BatchNormalization, InputLayer
+from keras.layers import Dense, BatchNormalization, InputLayer
 from keras.models import Model, Sequential
-from keras.regularizers import l2
 
 
 def create_model() -> Model:
     """Create and return a new instance of the fully connected network for processing time zero information"""
     # Create a neural network model that includes several dense layers with hyperbolic tangent activations, L2 regularization, and batch normalization
-    regularizer = l2(0)
-    dropout = 0
     activation = 'tanh'
     model = Sequential([
         InputLayer(input_shape=(9,)),
         BatchNormalization(),
-        Dense(24, activation=activation, kernel_regularizer=regularizer),
-        Dropout(dropout),
-        Dense(12, activation=activation, kernel_regularizer=regularizer),
-        Dropout(dropout),
-        Dense(6, activation=activation, kernel_regularizer=regularizer),
-        Dropout(dropout),
-        Dense(3, kernel_regularizer=regularizer)
+        Dense(24, activation=activation),
+        Dense(12, activation=activation),
+        Dense(6, activation=activation),
+        Dense(3)
     ])
     # Output a summary of the model's architecture
     print(model.summary())
