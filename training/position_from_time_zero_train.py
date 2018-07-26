6de9e48191bfee64a1f0264ff2d07dad497058ef
brendon-ai <brendon-m@outlook.com>
2018-07-26 15:14:23 -0400

    Save tests without correct network input

--- training/position_from_time_zero_train.py
+++ training/position_from_time_zero_train.py
@@ -2,6 +2,8 @@
 """A training script for a neural network that predicts the position of a bubble based on the times at which the audio signal arrives at various piezos"""
 # Created by Brendon Matusch, July 2018
 
+import os
+
 from data_processing.event_data_set import EventDataSet, RunType
 from data_processing.experiment_serialization import save_test
 from models.position_from_time_zero_network import create_model
@@ -21,10 +23,24 @@ event_data_set = EventDataSet(
 # Get the zero time arrays and corresponding position ground truths
 training_input, training_ground_truths, validation_input, validation_ground_truths = event_data_set.position_from_time_zero()
 
-# Train the model on the loaded data set for a certain number of epochs
-model.fit(
-    x=training_input,
-    y=training_ground_truths,
-    validation_data=(validation_input, validation_ground_truths),
-    epochs=1000
-)
+# Iterate for a certain number of epochs
+for epoch in range(1000):
+    # Train the model on the loaded data set
+    model.fit(
+        x=training_input,
+        y=training_ground_truths,
+        validation_data=(validation_input, validation_ground_truths),
+        epochs=1
+    )
+    # Run predictions on the validation data set, and save the experimental run
+    validation_network_outputs = model.predict(validation_input)
+    save_test(
+        event_data_set,
+        validation_ground_truths,
+        validation_network_outputs,
+        epoch=epoch,
+        prefix='zero_time_'
+    )
+    # Save the current model, named with the epoch number
+    model_path = os.path.expanduser(f'~/zero_time_epoch{epoch}.h5')
+    model.save(model_path)
