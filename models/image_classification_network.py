0f7c46956a986c48370b22aae03110f8a2810d2f
brendon-ai <brendon-m@outlook.com>
2018-07-27 16:51:21 -0400

    Added L2 regularization to all layers

--- models/image_classification_network.py
+++ models/image_classification_network.py
@@ -12,20 +12,21 @@ def create_model() -> Model:
     """Create and return a new instance of the image classification convolutional network"""
     # Calculate the number of images there are stacked along the channels axis
     channels = END_IMAGE_INDEX - START_IMAGE_INDEX
-    # Create a network with hyperbolic tangent activations and dropout regularization on the fully connected layers
+    # Create a network with hyperbolic tangent activations, dropout regularization on the fully connected layers, and L2 regularization everywhere
     activation = 'tanh'
     dropout = 0.25
+    regularizer = l2(0.01)
     model = Sequential([
         InputLayer(input_shape=(WINDOW_SIDE_LENGTH, WINDOW_SIDE_LENGTH, channels)),
         BatchNormalization(),
-        Conv2D(filters=16, kernel_size=4, strides=2, activation=activation),
-        Conv2D(filters=32, kernel_size=3, strides=2, activation=activation),
-        Conv2D(filters=32, kernel_size=3, strides=2, activation=activation),
-        Conv2D(filters=64, kernel_size=2, strides=1, activation=activation),
+        Conv2D(filters=16, kernel_size=4, strides=2, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=32, kernel_size=3, strides=2, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=32, kernel_size=3, strides=2, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=64, kernel_size=2, strides=1, activation=activation, kernel_regularizer=regularizer),
         Flatten(),
-        Dense(64, activation=activation),
+        Dense(64, activation=activation, kernel_regularizer=regularizer),
         Dropout(dropout),
-        Dense(16, activation=activation),
+        Dense(16, activation=activation, kernel_regularizer=regularizer),
         Dropout(dropout),
         Dense(1)
     ])
