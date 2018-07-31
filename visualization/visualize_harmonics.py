a79996eda3f92a79bf4819608102c4406fa3ace5
brendon-ai <brendon-m@outlook.com>
2018-07-31 08:35:25 -0400

    Created stub for harmonic visualization script

---
+++ visualization/visualize_harmonics.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+"""A script for loading audio from an event, removing lower frequencies so only harmonics are audible, and visualizing and saving it"""
+# Created by Brendon Matusch, July 2018
