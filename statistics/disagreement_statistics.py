181add179757937eb5e57b9cebd8a3b3b9647c89
brendon-ai <brendon-m@outlook.com>
2018-07-31 07:20:18 -0400

    Find the events on which they disagree

--- statistics/disagreement_statistics.py
+++ statistics/disagreement_statistics.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
 """A script to analyze a saved validation set, find the events on which the AP and neural network disagree, and print print information about those events"""
 # Created by Brendon Matusch, July 2018
+
+import sys
+
+import numpy as np
+
+from data_processing.experiment_serialization import load_test
+from utilities.verify_arguments import verify_arguments
+
+# Verify that a path to the JSON data file is passed
+verify_arguments('JSON data file')
+# Load the data set from the file
+events, ground_truths, network_outputs = load_test(sys.argv[1])
+# Get the acoustic parameter data from the events
+acoustic_parameters = [event.logarithmic_acoustic_parameter for event in events]
+# Convert the network's outputs to binary predictions (as a NumPy array)
+network_predictions = np.array([output >= 0.5 for output in network_outputs])
+# Repeat with acoustic parameters, dividing at a specific threshold
+acoustic_predictions = np.array([acoustic_parameter >= 0.22 for acoustic_parameter in acoustic_parameters])
+# Get the indices of the bubbles on which the network and the acoustic parameter disagree
+disagreement_indices = np.argwhere(network_predictions != acoustic_predictions)
+# Get the events on which the two techniques disagree (indices are wrapped in single-element arrays)
+disagreement_events = [events[index[0]] for index in disagreement_indices]
+print(len(disagreement_events))
