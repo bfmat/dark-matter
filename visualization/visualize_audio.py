783fa7b904d9009f38d9b67b7eee25a7f8b77b92
brendon-ai <brendon-m@outlook.com>
2018-07-31 07:36:38 -0400

    Switched audio visualizer to take a unique index as input

--- visualization/visualize_audio.py
+++ visualization/visualize_audio.py
@@ -8,26 +8,31 @@ import sys
 import time
 
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.io import wavfile
 
 from data_processing.audio_domain_processing import time_to_frequency_domain
-from data_processing.bubble_data_point import load_bubble_audio
+from data_processing.bubble_data_point import load_bubble_audio, load_bubble_frequency_domain
 from data_processing.event_data_set import EventDataSet
 from utilities.verify_arguments import verify_arguments
 
 # The number of samples per second to use when saving the audio as WAV files
 SAMPLE_RATE = 25_000
 
-# A path to a binary audio file is required
-verify_arguments('binary file path')
+# A unique bubble index is required
+verify_arguments('unique bubble index')
 
-# Load the audio file located at the path provided
-file_path = os.path.expanduser(sys.argv[1])
-bubble_audio_list = load_bubble_audio(None, file_path)
-# Otherwise, get the first and only element of the list: a NumPy array containing the audio in time domain
-time_domain = bubble_audio_list[0]
-# Run a Fourier transform on the audio to get it in the frequency domain
-frequency_domain = time_to_frequency_domain(time_domain)
+# Load that one bubble from the data file
+identifier = int(sys.argv[1])
+bubble = EventDataSet.load_specific_indices([identifier])[0]
+# Get the audio information associated with the bubble (taking the first and only element of the list)
+time_domain = load_bubble_audio(bubble)[0]
+print(time_domain.shape)
+# Also, get the flattened full-resolution Fourier transform in the same way
+frequency_domain_flat = load_bubble_frequency_domain(bubble, banded=False)[0]
+# Reshape the frequency domain into an array with 2 channels
+frequency_domain = np.reshape(frequency_domain_flat, (-1, 2))
+print(frequency_domain.shape)
 
 # Iterate over the channel indices, which correspond to the row indices in the graph
 channels = time_domain.shape[1]
