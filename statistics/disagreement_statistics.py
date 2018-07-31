7c28313159339546bd7b093c08294c3e76f7f755
brendon-ai <brendon-m@outlook.com>
2018-07-31 06:49:09 -0400

    Created stub for disagreement statistics script

---
+++ statistics/disagreement_statistics.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+"""A script to analyze a saved validation set, find the events on which the AP and neural network disagree, and print print information about those events"""
+# Created by Brendon Matusch, July 2018
