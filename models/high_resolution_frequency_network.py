634d5fb951ec7b70dcc8b7fe7a16acd4c19e12c2
Brendon Matusch <brendon-m@outlook.com>
2018-08-01 15:10:48 -0400

    Added data for flaming disaster with custom FFT

--- models/high_resolution_frequency_network.py
+++ models/high_resolution_frequency_network.py
@@ -10,7 +10,7 @@ def create_model() -> Model:
     """Create and return a new instance of the high-resolution frequency network"""
     # Create a neural network composed of dense layers with dropout and L2 regularization, using hyperbolic tangent activations
     activation = 'tanh'
-    regularizer = l2(0.003)
+    regularizer = l2(0.0003)
     dropout = 0
     # Create two inputs, one for the audio data and one for the position, and concatenate them together
     audio_input = Input((100_002,))
@@ -19,7 +19,7 @@ def create_model() -> Model:
     x = Dropout(dropout)(x)
     x = Dense(8, activation=activation, kernel_regularizer=regularizer)(x)
     x = Dropout(dropout)(x)
-    output = Dense(1, activation='sigmoid', kernel_regularizer=regularizer)(x)
+    output = Dense(1, kernel_regularizer=regularizer)(x)
     # Create a model with both inputs
     model = Model(inputs=audio_input, outputs=output)
     # Output a summary of the model's architecture
