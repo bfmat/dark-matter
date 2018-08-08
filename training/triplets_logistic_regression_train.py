a9e460330e8171b2b568d564d6ed63920eeb9147
brendon-ai <brendon-m@outlook.com>
2018-08-08 18:36:00 -0400

    Added validation split

--- training/triplets_logistic_regression_train.py
+++ training/triplets_logistic_regression_train.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 from data_processing.load_triplet_classification_data import load_triplet_classification_data
 
@@ -13,7 +14,13 @@ loud_events, quiet_events = load_triplet_classification_data()
 inputs = np.stack([event.banded_frequency_domain.flatten() for event in loud_events + quiet_events])
 # Make a binary output list, where 1 is loud and 0 is quiet
 outputs = np.concatenate([np.ones(len(loud_events)), np.zeros(len(quiet_events))])
+# Split the inputs and outputs into training and validation sets
+training_inputs, validation_inputs, training_outputs, validation_outputs = train_test_split(inputs, outputs, test_size=0.15)
 # Create a logistic regression classifier
 classifier = LogisticRegression()
-# Train it on the input and output arrays
-classifier.fit(X=inputs, y=outputs)
+# Fit the classifier on the training input and output arrays
+classifier.fit(training_inputs, training_outputs)
+# Score the classifier on the validation arrays
+score = classifier.score(validation_inputs, validation_outputs)
+# Output the score to the user
+print('Validation score:', score)
