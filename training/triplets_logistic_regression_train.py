cc48e4201d7c61c85d01f571cb8eb5d7f26e885f
brendon-ai <brendon-m@outlook.com>
2018-08-08 19:39:32 -0400

    Crop input data; print training as well as validation score

--- training/triplets_logistic_regression_train.py
+++ training/triplets_logistic_regression_train.py
@@ -5,13 +5,15 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
 
 from data_processing.load_triplet_classification_data import load_triplet_classification_data
 
 # Load the triplet events
 loud_events, quiet_events = load_triplet_classification_data()
 # Stack an input array of flattened position-corrected 8-band Fourier transforms for all of the events together
-inputs = np.stack([event.banded_frequency_domain.flatten() for event in loud_events + quiet_events])
+inputs = np.stack([event.banded_frequency_domain[1:, :, 2].flatten() for event in loud_events + quiet_events])
+inputs = normalize(inputs)
 # Make a binary output list, where 1 is loud and 0 is quiet
 outputs = np.concatenate([np.ones(len(loud_events)), np.zeros(len(quiet_events))])
 # Split the inputs and outputs into training and validation sets
@@ -20,7 +22,9 @@ training_inputs, validation_inputs, training_outputs, validation_outputs = train
 classifier = LogisticRegression()
 # Fit the classifier on the training input and output arrays
 classifier.fit(training_inputs, training_outputs)
-# Score the classifier on the validation arrays
-score = classifier.score(validation_inputs, validation_outputs)
-# Output the score to the user
-print('Validation score:', score)
+# Score the classifier on the training and validation arrays separately
+training_score = classifier.score(training_inputs, training_outputs)
+validation_score = classifier.score(validation_inputs, validation_outputs)
+# Output the scores to the user
+print('Training score:', training_score)
+print('Validation score:', validation_score)
