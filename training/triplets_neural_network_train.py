99525b95579f10843069a658755929ff06c79344
Brendon Matusch <brendon-m@outlook.com>
2018-08-09 07:26:05 -0400

    Shuffle list; results are not so good anymore

--- training/triplets_neural_network_train.py
+++ training/triplets_neural_network_train.py
@@ -2,6 +2,8 @@
 """A script for training a neural network model to separate loud from quiet triplet events"""
 # Created by Brendon Matusch, August 2018
 
+import random
+
 import numpy as np
 from keras.layers import Dense, Dropout, BatchNormalization, InputLayer
 from keras.models import Model, Sequential
@@ -11,14 +13,17 @@ from data_processing.load_triplet_classification_data import load_triplet_classi
 
 # Load the triplet events
 loud_events, quiet_events = load_triplet_classification_data()
+# Combine them into one list and shuffle it
+all_events = loud_events + quiet_events
+random.shuffle(all_events)
 # Stack an input array of flattened position-corrected 8-band Fourier transforms for all of the events together
-inputs = np.stack([event.banded_frequency_domain[1:, :, 2].flatten() for event in loud_events + quiet_events])
+inputs = np.stack([event.banded_frequency_domain[1:, :, 2].flatten() for event in all_events])
 # Make a binary output list, where 1 is loud and 0 is quiet
 outputs = np.concatenate([np.ones(len(loud_events)), np.zeros(len(quiet_events))])
 
 # Create a neural network model that includes several dense layers with hyperbolic tangent activations, L2 regularization, and batch normalization
 regularizer = l2(0)
-dropout = 0.25
+dropout = 0
 model = Sequential([
     InputLayer(input_shape=(16,)),
     BatchNormalization(),
@@ -35,4 +40,4 @@ model.compile(
     metrics=['accuracy']
 )
 # Train the model, setting aside 15% of the data for validation
-model.fit(inputs, outputs, epochs=200, validation_split=0.2)
+model.fit(inputs, outputs, epochs=200, validation_split=0.25)
