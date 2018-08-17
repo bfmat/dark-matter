16b6c587f0a20335da493217ffe950454dc549ae
Brendon Matusch <brendon-m@outlook.com>
2018-08-17 10:06:30 -0400

    Removed unnecessary rotations; corrected clockwise to anticlockwise

--- data_processing/surface_topology.py
+++ data_processing/surface_topology.py
@@ -10,9 +10,11 @@ import numpy as np
 class SurfaceTopologyNode:
     """A single topology node, which contains information loaded from one dictionary in the JSON file"""
 
-    # A placeholder for references to the nodes above, right and left (triangular directions), in clockwise order
-    # The first node (above) is guaranteed to be defined (even if it is physically below this node); the other 2 are defined if possible
-    connected_nodes = []
+    # A placeholder for references to the nodes above, right and left (triangular directions), in anticlockwise order, starting with any of the 3; any 0, 1 or 2 of these may be undefined
+    connected_nodes = None
+
+    # A placeholder for the index, in the list of references to the nodes, to the vertically highest connected node
+    highest_node_index = None
 
     def __init__(self, json_dictionary):
         """Load the relevant components from a JSON dictionary corresponding to a node"""
@@ -21,8 +23,8 @@ class SurfaceTopologyNode:
         # Get the 3 values in the position individually (Y is vertical)
         self.x_position, self.y_position, self.z_position = json_dictionary['position']
         # Take the raw list of connections for now; up, right, and left will be calculated later
-        # They are defined to be in clockwise order, starting with any of the 3
-        self.raw_connections_clockwise = json_dictionary['connections']
+        # They are defined to be in anticlockwise order, starting with any of the 3
+        self.raw_connections_anticlockwise = json_dictionary['connections']
         # Take the list of values directly; the topology set class will handle processing when the data is loaded
         self.values = json_dictionary['values']
 
@@ -40,20 +42,17 @@ class SurfaceTopologySet:
         # Iterate over the nodes, calculating their relationships to other nodes
         for node in self.nodes:
             # Get the nodes this one is connected to
-            connected_nodes_clockwise = [
+            node.connected_nodes = [
                 # Find the node with this ID rather than just using the index; they may be out of order
                 [other_node for other_node in self.nodes if other_node.identifier == node_identifier][0]
                 # Let None pass through; don't try to load anything in that case
                 if node_identifier is not None else None
                 # The identifiers are in the original order
-                for node_identifier in node.raw_connections_clockwise
+                for node_identifier in node.raw_connections_anticlockwise
             ]
             # Get the Y (vertical) positions of these connected nodes, replacing None with negative infinity (so empty connections will never be considered the farthest up)
-            y_positions = [connected_node.y_position if connected_node is not None else float('-inf') for connected_node in connected_nodes_clockwise]
-            # Get the index of the node with the greatest vertical position
+            y_positions = [connected_node.y_position if connected_node is not None else float('-inf') for connected_node in node.connected_nodes]
+            # Find the index of the node with the greatest vertical position
             # That may be below this node if it is at the top of a convex surface
             # This specific edge case will have to be handled when calculating kernels
-            highest_node_index = np.argmax(y_positions)
-            # Define the properly ordered list of connections (where the first is vertically the highest) by offsetting each of the node indices
-            # The goal is to rotate the list of connections to the top node comes first
-            node.connected_nodes = [connected_nodes_clockwise[(highest_node_index + index_offset) % len(connected_nodes_clockwise)] for index_offset in range(len(connected_nodes_clockwise))]
+            node.highest_node_index = np.argmax(y_positions)
