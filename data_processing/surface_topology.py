c6432978af41217ce3636b9a234243f5ce5e6daa
Brendon Matusch <brendon-m@outlook.com>
2018-08-17 08:18:34 -0400

    Successfully reordering list to put top node first

--- data_processing/surface_topology.py
+++ data_processing/surface_topology.py
@@ -4,14 +4,15 @@
 import json
 import os
 
+import numpy as np
+
 
 class SurfaceTopologyNode:
     """A single topology node, which contains information loaded from one dictionary in the JSON file"""
 
-    # Placeholders for references to the nodes above, right and left (triangular directions)
-    node_above = None
-    node_right = None
-    node_left = None
+    # A placeholder for references to the nodes above, right and left (triangular directions), in clockwise order
+    # The first node (above) is guaranteed to be defined (even if it is physically below this node); the other 2 are defined if possible
+    connected_nodes = []
 
     def __init__(self, json_dictionary):
         """Load the relevant components from a JSON dictionary corresponding to a node"""
@@ -38,13 +39,21 @@ class SurfaceTopologySet:
         self.nodes = [SurfaceTopologyNode(json_dictionary) for json_dictionary in json_data]
         # Iterate over the nodes, calculating their relationships to other nodes
         for node in self.nodes:
-            # Get the nodes this one is connected to, leaving None as it is
-            connected_nodes_clockwise = [self.nodes[node_index] if node_index is not None else None for node_index in node.raw_connections_clockwise]
-            # Get the Y (vertical) positions of these connected nodes
-            # TODO
-            print(connected_nodes_clockwise)
-
-
-
-# Temporary: load the example JSON file
-topology = SurfaceTopologySet('example_surface_topology_set.json')
+            # Get the nodes this one is connected to
+            connected_nodes_clockwise = [
+                # Find the node with this ID rather than just using the index; they may be out of order
+                [other_node for other_node in self.nodes if other_node.identifier == node_identifier][0]
+                # Let None pass through; don't try to load anything in that case
+                if node_identifier is not None else None
+                # The identifiers are in the original order
+                for node_identifier in node.raw_connections_clockwise
+            ]
+            # Get the Y (vertical) positions of these connected nodes, replacing None with negative infinity (so empty connections will never be considered the farthest up)
+            y_positions = [connected_node.y_position if connected_node is not None else float('-inf') for connected_node in connected_nodes_clockwise]
+            # Get the index of the node with the greatest vertical position
+            # That may be below this node if it is at the top of a convex surface
+            # This specific edge case will have to be handled when calculating kernels
+            highest_node_index = np.argmax(y_positions)
+            # Define the properly ordered list of connections (where the first is vertically the highest) by offsetting each of the node indices
+            # The goal is to rotate the list of connections to the top node comes first
+            node.connected_nodes = [connected_nodes_clockwise[(highest_node_index + index_offset) % len(connected_nodes_clockwise)] for index_offset in range(len(connected_nodes_clockwise))]
