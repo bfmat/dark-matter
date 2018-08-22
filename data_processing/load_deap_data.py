1904428712abd5cd9ce49dd8018e134e39119df2
Brendon Matusch <brendon-m@outlook.com>
2018-08-22 11:12:25 -0400

    Created code for loading file

---
+++ data_processing/load_deap_data.py
@@ -0,0 +1,15 @@
+"""Load the DEAP Monte Carlo simulation data from the Joblib file"""
+# Created by Brendon Matusch, August 2018
+
+import os
+from typing import List, Tuple
+
+import numpy as np
+from sklearn.externals import joblib
+
+
+def load_deap_data() -> Tuple[List[Tuple[np.ndarray, np.ndarray, List[List[float]]]], List[Tuple[np.ndarray, np.ndarray, List[List[float]]]]]:
+    """Load and return the DEAP Monte Carlo simulation data"""
+    # Use Joblib to load the binary file, and return it directly
+    with open(os.path.expanduser('~/deap_data.pkl'), 'rb') as joblib_file:
+        return joblib.load(joblib_file)
