Clarified/corrected recall calculation (I think it's equivalent, but clearer anyway)

visualization/graph_binary_predictions.py
@@ -49,10 +49,10 @@ for criterion_data, criterion_name, classification_threshold in zip(
     predicted_recoils = np.count_nonzero(classifications == 0)
     erroneous_recoils = np.count_nonzero(np.logical_and(classifications == 0, ground_truths == 1))
     print(f'Precision of {(predicted_recoils - erroneous_recoils) / predicted_recoils} for {criterion_name}')
-    # Repeat for the number of events falsely predicted as alphas, which are actually nuclear recoils (possibly representing WIMP candidates in real data)
-    predicted_alphas = np.count_nonzero(classifications == 1)
+    # Repeat for the proportion of neutron calibration events that are correctly predicted as recoils; this represents not the purity, but the number missed
+    actual_recoils = np.count_nonzero(classifications == 1)
     erroneous_alphas = np.count_nonzero(np.logical_and(classifications == 1, ground_truths == 0))
-    print(f'Recall of {(predicted_alphas - erroneous_alphas) / predicted_alphas} for {criterion_name}')
+    print(f'Recall of {(actual_recoils - erroneous_alphas) / actual_recoils} for {criterion_name}')
     # Divide all of these data points by their overall standard deviation, to normalize their range
     criterion_data /= np.std(criterion_data)
     # Create a list for the 2 standard deviations