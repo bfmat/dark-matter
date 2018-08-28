a6a461074a03e643509a5f45ef53737b3ae16929
Brendon Matusch <brendon-m@outlook.com>
2018-08-28 12:31:06 -0400

    Created stub for mean AP accuracy calculator

---
+++ statistics/mean_ap_accuracy_across_configurations.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+"""Given standard input from the AP similarity statistic calculation script, compute the mean accuracy over each of the multiple runs for every configuration"""
+# Created by Brendon Matusch, August 2018
