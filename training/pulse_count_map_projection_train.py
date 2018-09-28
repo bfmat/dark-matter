8d2ea61ee8afa32eda10bcebae02c806e8e2b95e
Brendon Matusch <brendon-m@outlook.com>
2018-09-28 14:11:56 -0400

    Fixed issues with map projections; created training script

---
+++ training/pulse_count_map_projection_train.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Train a fully connected neural network on the numbers of pulses for each PMT in the DEAP data"""
+# Created by Brendon Matusch, August 2018
+
+import numpy as np
+
+from data_processing.deap_serialization import save_test
+from data_processing.load_deap_data import load_real_world_deap_data, load_simulated_deap_data
+from data_processing.pmt_map_projection import pmt_map_projection
+from models.pulse_count_network import create_model
+from training.pulse_count_train import prepare_events, evaluate_predictions
+
+# The number of events to set aside for validation
+VALIDATION_SIZE = 2000
+
+
+# Load all simulated events from the file
+neck_events, non_neck_events = load_simulated_deap_data()
+# Project the pulse counts onto a map, wrapping in a single-element tuple so the preprocessing function will work
+neck_events_map, non_neck_events_map = [[(pmt_map_projection(event[0]),) for event in events] for events in [neck_events, non_neck_events]]
+# Convert them to NumPy arrays for training (also getting the reordered list of events)
+inputs, ground_truths, events = prepare_events(neck_events_map, non_neck_events_map)
+# Split the inputs and ground truths into training and validation sets
+validation_inputs, training_inputs = np.split(inputs, [VALIDATION_SIZE])
+validation_ground_truths, training_ground_truths = np.split(ground_truths, [VALIDATION_SIZE])
+# Split the events correspondingly (NumPy cannot be used on a list)
+# Take only the validation events, which are located at the beginning of the list
+validation_events = events[:VALIDATION_SIZE]
+
+# Load all real-world test events from the file
+real_world_neck_events, real_world_neutron_events = load_real_world_deap_data()
+# Prepare the input and ground truth data for testing
+test_inputs, test_ground_truths, test_events = prepare_events(real_world_neck_events, real_world_neutron_events)
+
+# Create an instance of the neural network model
+model = create_model()
+# Iterate for a certain number of epochs
+for epoch in range(100):
+    # Train the model for a single epoch
+    model.fit(inputs, ground_truths, validation_data=(validation_inputs, validation_ground_truths))
+    # Run predictions on the validation set with the trained model, removing the single-element second axis
+    validation_predictions = model.predict(validation_inputs)[:, 0]
+    # Evaluate the network's predictions, printing statistics and saving a JSON file
+    evaluate_predictions(validation_ground_truths, validation_predictions, validation_events, epoch, set_name='validation')
+    # Repeat this process for the dedicated test set
+    test_predictions = model.predict(test_inputs)[:, 0]
+    evaluate_predictions(test_ground_truths, test_predictions, test_events, epoch, set_name='real_world_test')
