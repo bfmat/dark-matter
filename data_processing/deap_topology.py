fd2c8f0ba900a646366dbfd555fd7f7430b716d5
Brendon Matusch <brendon-m@outlook.com>
2018-10-01 21:09:05 -0400

    Calculate distances to other PMTs

--- data_processing/deap_topology.py
+++ data_processing/deap_topology.py
@@ -18,10 +18,15 @@ def create_deap_topology() -> SurfaceTopologySet:
     pmt_dictionaries = [{'id': pmt_index, 'position': position_3d} for pmt_index, position_3d in enumerate(zip(X_POSITIONS, Y_POSITIONS, Z_POSITIONS))]
     # Create an array out of the pulse counts for all PMTs
     all_pulse_counts = np.array([event[0] for event in neck_events + non_neck_events])
-    # Iterate over the PMT indices, handling all data for that PMT at once
+    # Calculate the vertically highest Z position (which represents the top edge of the detector)
+    top_edge = max(pmt_dictionary['position'][2] for pmt_dictionary in pmt_dictionaries)
+    # Iterate over the PMT indices, handling all data and connections for that PMT at once
     for pmt_index in range(len(pmt_dictionaries)):
         # Get the pulse counts for all examples corresponding to this PMT, and set the field of the corresponding dictionary
         pmt_dictionaries[pmt_index]['values'] = list(all_pulse_counts[:, pmt_index])
+        # Calculate the straight-line distance from this PMT to all of the other PMTs
+        position = np.array(pmt_dictionaries[pmt_index]['position'])
+        pmt_distances = [np.linalg.norm(position - np.array(comparison_pmt['position'])) for comparison_pmt in pmt_dictionaries]
 
 
 # Temporary: run the function for debugging purposes
