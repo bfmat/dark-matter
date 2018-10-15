abcb900a9be87571eba0938303be448ade454a97
brendon-ai <brendon-m@outlook.com>
2018-10-14 20:07:10 -0400

    Print accuracy for AP triplet classification

--- visualization/visualize_triplets.py
+++ visualization/visualize_triplets.py
@@ -3,6 +3,7 @@
 # Created by Brendon Matusch, August 2018
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from data_processing.load_triplet_classification_data import load_triplet_classification_data
 
@@ -12,6 +13,14 @@ loud_events, quiet_events = load_triplet_classification_data()
 all_events = loud_events + quiet_events
 ap_values = [event.logarithmic_acoustic_parameter for event in all_events]
 neural_network_scores = [event.original_neural_network_score for event in all_events]
+# Convert the AP values to binary predictions as to whether the event is loud or quiet
+ap_predictions = np.array(ap_values) > 1.1
+# Get an array of the ground truths for this data
+ground_truths = np.concatenate([np.ones(len(loud_events)), np.zeros(len(quiet_events))])
+# Calculate how often they agree (how many examples AP is correct on)
+agreements = np.count_nonzero(ap_predictions == ground_truths)
+# Calculate and print the resulting accuracy value
+print('AP accuracy:', agreements / len(all_events))
 # Create a list of colors where the loud events are red and the quiet events are blue
 colors = (['r'] * len(loud_events)) + (['b'] * len(quiet_events))
 # Scatter plot the events with AP on the X axis, neural network predictions on the Y axis, and corresponding colors
