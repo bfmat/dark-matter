69eb9d207c188db01de24b0bcd3a38ae872156dc
brendon-ai <brendon-m@outlook.com>
2018-10-27 13:30:50 -0400

    Added L2 to CNN

--- models/map_projection_cnn.py
+++ models/map_projection_cnn.py
@@ -3,20 +3,22 @@
 
 from keras.layers import Conv2D, Dense, Flatten, Dropout, BatchNormalization, InputLayer
 from keras.models import Model, Sequential
+from keras.regularizers import l2
 
 
 def create_model() -> Model:
     """Create and return a fully connected neural network for pulse count information"""
     # Create a neural network model that includes several convolutional and dense layers with hyperbolic tangent activations
     activation = 'tanh'
+    regularizer = l2(0.001)
     model = Sequential([
         InputLayer(input_shape=(10, 35, 1)),
         BatchNormalization(),
-        Conv2D(filters=8, kernel_size=3, strides=2, activation=activation),
-        Conv2D(filters=8, kernel_size=2, strides=2, activation=activation),
+        Conv2D(filters=8, kernel_size=3, strides=2, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=8, kernel_size=2, strides=2, activation=activation, kernel_regularizer=regularizer),
         Flatten(),
-        Dense(8, activation=activation),
-        Dense(1, activation='sigmoid')
+        Dense(8, activation=activation, kernel_regularizer=regularizer),
+        Dense(1, activation='sigmoid', kernel_regularizer=regularizer)
     ])
     # Output a summary of the model's architecture
     print(model.summary())
