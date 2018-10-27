ba396dba30bbd6391957807952bec061f76a4318
brendon-ai <brendon-m@outlook.com>
2018-10-27 11:59:04 -0400

    Calculate mean statistics for map projection

--- training/pulse_count_map_projection_train.py
+++ training/pulse_count_map_projection_train.py
@@ -13,6 +13,8 @@ from training.pulse_count_train import prepare_events, evaluate_predictions
 # The number of events to set aside for validation
 VALIDATION_SIZE = 500
 
+# The number of epochs to train for
+EPOCHS = 100
 
 # Load all simulated events from the file
 neck_events, non_neck_events = load_simulated_deap_data()
@@ -34,16 +36,26 @@ real_neck_events_map, real_neutron_events_map = [[(pmt_map_projection(event[0]),
 # Prepare the input and ground truth data for testing
 test_inputs, test_ground_truths, test_events = prepare_events(real_neck_events_map, real_neutron_events_map)
 
-# Create an instance of the neural network model
-model = create_model()
-# Iterate for a certain number of epochs
-for epoch in range(100):
-    # Train the model for a single epoch
-    model.fit(training_inputs, training_ground_truths, validation_data=(validation_inputs, validation_ground_truths), class_weight={0: 0.005, 1: 1.0})
-    # Run predictions on the validation set with the trained model, removing the single-element second axis
-    validation_predictions = model.predict(validation_inputs)[:, 0]
-    # Evaluate the network's predictions, printing statistics and saving a JSON file
-    evaluate_predictions(validation_ground_truths, validation_predictions, validation_events, epoch, set_name='validation')
-    # Repeat this process for the dedicated test set
-    test_predictions = model.predict(test_inputs)[:, 0]
-    evaluate_predictions(test_ground_truths, test_predictions, test_events, epoch, set_name='real_world_test')
+# Create a list to hold the numbers of (false and true) (positives and negatives) for each training run
+performance_statistics = []
+# Train the network multiple times to get an idea of the general accuracy
+for _ in range(3):
+    # Create an instance of the neural network model
+    model = create_model()
+    # Iterate for a certain number of epochs
+    for epoch in range(EPOCHS):
+        # Train the model for a single epoch
+        model.fit(training_inputs, training_ground_truths, validation_data=(validation_inputs, validation_ground_truths), class_weight={0: 0.01, 1: 1.0})
+        # Run predictions on the validation set with the trained model, removing the single-element second axis
+        validation_predictions = model.predict(validation_inputs)[:, 0]
+        # Evaluate the network's predictions and add the statistics to the list, only if we are in the last few epochs (we don't care about the other ones, it is still learning then)
+        if epoch >= EPOCHS - 10:
+            performance_statistics.append(evaluate_predictions(validation_ground_truths, validation_predictions, validation_events, epoch, set_name='validation'))
+# Add up each of the statistics for the last few epochs and calculate the mean
+statistics_mean = np.mean(np.stack(performance_statistics, axis=0), axis=0)
+# Using these values, calculate and print the percentage of neck alphas removed, and the percentage of nuclear recoils incorrectly removed alongside them
+true_positives, true_negatives, false_positives, false_negatives = statistics_mean
+neck_alphas_removed = true_positives / (true_positives + false_negatives)
+nuclear_recoils_removed = false_positives / (false_positives + true_negatives)
+print('Neck alphas removed:', neck_alphas_removed)
+print('Nuclear recoils removed:', nuclear_recoils_removed)
