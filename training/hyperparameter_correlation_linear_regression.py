e4992d3dbf127e8601f3b29b228371e667c57590
brendon-ai <brendon-m@outlook.com>
2018-10-27 21:33:33 -0400

    Prepared hyper correlation script for gravitational training

--- training/hyperparameter_correlation_linear_regression.py
+++ training/hyperparameter_correlation_linear_regression.py
@@ -6,13 +6,12 @@ import sys
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
 
 # The hyperparameters that we are concerned with using to predict accuracy
-HYPERPARAMETERS = ['gravity_multiplier_increment', 'learning_rate', 'distortion_power']
+HYPERPARAMETERS = ['definitive_training_examples', 'gravity_multiplier_increment', 'learning_rate', 'distortion_power']
 
-# Read all lines provided to standard input (from the script that calculates mean by configuration) and strip whitespace
-lines = [line.strip() for line in sys.stdin.readlines()]
+# Read all configuration lines provided to standard input (from the script that calculates mean by configuration) and strip whitespace
+lines = [line.strip() for line in sys.stdin.readlines() if 'Configuration:' in line]
 # Get the configuration identifier from each line
 configurations = [line.split()[1] for line in lines]
 # Create a NumPy array that will contain values of the hyperparameters
@@ -24,13 +23,11 @@ for configuration_index, configuration in enumerate(configurations):
     for hyperparameter_index, hyperparameter in enumerate(HYPERPARAMETERS):
         # Split the line by the name of the hyperparameter, and then get the hyperparameter value (as a floating-point number) up to the next slash separator
         hyperparameter_values[configuration_index, hyperparameter_index] = float(configuration.split(hyperparameter)[1].split('/')[0])
-# Add polynomial features up to degree 3 to the hyperparameter matrix
-hyperparameter_polynomials = PolynomialFeatures(degree=3).fit_transform(hyperparameter_values)
 # Extract the accuracy value from each line
 accuracy_values = [float(line.split()[3]) for line in lines]
 # Create a linear regression classifier and train it to predict the accuracy values based on the hyperparameters including polynomials
 linear_regression = LinearRegression()
-linear_regression.fit(hyperparameter_polynomials, accuracy_values)
+linear_regression.fit(hyperparameter_values, accuracy_values)
 # Print the classifier's score on its training set to get an idea of the correlation between the hyperparameters and accuracy
-score = linear_regression.score(hyperparameter_polynomials, accuracy_values)
+score = linear_regression.score(hyperparameter_values, accuracy_values)
 print('Score:', score)
