78dacbb34f6ce861e8d3951c7c80fd6fe83d99e4
brendon-ai <brendon-m@outlook.com>
2018-11-03 19:01:57 -0400

    Revert "Display best image CNN architecture"
    This reverts commit 87c841b038208589e084059c069187fcff522885.

--- models/image_classification_network.py
+++ models/image_classification_network.py
@@ -4,7 +4,6 @@
 from keras.layers import Conv2D, Flatten, Dropout, InputLayer, BatchNormalization, Dense
 from keras.models import Model, Sequential
 from keras.regularizers import l2
-from keras.utils import plot_model
 
 from data_processing.bubble_data_point import WINDOW_SIDE_LENGTH, START_IMAGE_INDEX, END_IMAGE_INDEX
 
@@ -15,15 +14,21 @@ def create_model() -> Model:
     channels = END_IMAGE_INDEX - START_IMAGE_INDEX
     # Create a network with hyperbolic tangent activations, dropout regularization on the fully connected layers, and L2 regularization everywhere
     activation = 'tanh'
-    dropout = 0
-    regularizer = l2(0)
+    dropout = 0.25
+    regularizer = l2(0.01)
     model = Sequential([
         InputLayer(input_shape=(WINDOW_SIDE_LENGTH, WINDOW_SIDE_LENGTH, channels)),
         BatchNormalization(),
         Conv2D(filters=32, kernel_size=4, strides=2, activation=activation, kernel_regularizer=regularizer),
         Conv2D(filters=32, kernel_size=4, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=32, kernel_size=4, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=32, kernel_size=4, activation=activation, kernel_regularizer=regularizer),
         Conv2D(filters=64, kernel_size=3, activation=activation, kernel_regularizer=regularizer),
         Conv2D(filters=64, kernel_size=3, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=64, kernel_size=3, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=64, kernel_size=3, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=128, kernel_size=2, activation=activation, kernel_regularizer=regularizer),
+        Conv2D(filters=128, kernel_size=2, activation=activation, kernel_regularizer=regularizer),
         Conv2D(filters=128, kernel_size=2, activation=activation, kernel_regularizer=regularizer),
         Conv2D(filters=128, kernel_size=2, activation=activation, kernel_regularizer=regularizer),
         Flatten(),
@@ -43,7 +48,3 @@ def create_model() -> Model:
     )
     # Return the untrained model
     return model
-
-
-model = create_model()
-plot_model(model, to_file='/home/brendonm/model.png')
