a741dc12336332db0bae47e2ddc1643c5b37ae17
brendon-ai <brendon-m@outlook.com>
2018-11-08 22:05:49 -0500

    Made nice, working script for pulse count analysis

--- statistics/pulse_count_general_statistics.py
+++ statistics/pulse_count_general_statistics.py
@@ -5,8 +5,19 @@
 import json
 import sys
 
-import numpy as np
+# Parse the data list from standard input as JSON
+data = json.load(sys.stdin)
+# Calculate the minimum false positive percentage for runs with 100% recall
+# Recall refers to the number of alphas identified as such; false positive percentage refers to the number of recoils mis-identified as alphas
+min_false_perfect_recall = min([false_positive for (recall, false_positive) in data if recall == 1])
+print('Minimum false positives of runs with perfect recall:', min_false_perfect_recall)
+# Repeat this for runs with at least 97% recall
+min_false_97_recall = min([false_positive for (recall, false_positive) in data if recall >= 0.97])
+print('Minimum false positives of runs with at least 97% recall:', min_false_97_recall)
 
-# Parse the data list from standard input as JSON and convert it to a NumPy array
-data = np.array(json.load(sys.stdin))
-print(data)
+# Print the maximum recall of runs with at most 87% false positives
+max_recall_87_false = max([recall for (recall, false_positive) in data if false_positive <= 0.87])
+print('Maximum recall of runs with at most 87% false positives:', max_recall_87_false)
+# Repeat this for runs with at most 70% false positives
+max_recall_70_false = max([recall for (recall, false_positive) in data if false_positive <= 0.7])
+print('Maximum recall of runs with at most 70% false positives:', max_recall_70_false)
