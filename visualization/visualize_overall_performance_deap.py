7cdcdca21a31028cab504ead73a8bca1ed73e618
brendon-ai <brendon-m@outlook.com>
2018-11-10 13:44:30 -0500

    Visualize recall for false positives <= 87%

--- visualization/visualize_overall_performance_deap.py
+++ visualization/visualize_overall_performance_deap.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 
 # Create lists containing the performance statistics as well as the names of the configurations
-PERFORMANCE_VALUES = [87, 58.7, 58.8, 54.4]
-# PERFORMANCE_VALUES = [97, 99.1, 100, 99.3]
+# PERFORMANCE_VALUES = [87, 58.7, 58.8, 54.4]
+PERFORMANCE_VALUES = [97, 99.1, 100, 99.3]
 CONFIGURATIONS = ['Conventional Analysis', 'Dense Neural Network', 'Map Projection CNN', 'Topological CNN']
 # Color and legend the entries according to whether they are supervised or semi-supervised learning
 COLORS = ['gray', 'green', 'green', 'green']
@@ -21,10 +21,10 @@ ax.yaxis.set_major_formatter(mtick.PercentFormatter())
 plt.bar(range(len(PERFORMANCE_VALUES)), PERFORMANCE_VALUES, color=COLORS)
 # Now set the configuration names, with words angled so they fit
 plt.xticks(range(len(CONFIGURATIONS)), CONFIGURATIONS, rotation=10)
-# # Start at 90% so the difference is more obvious
-# plt.ylim(90, 100)
+# Start at 90% so the difference is more obvious; go slightly above 100 so there is space
+plt.ylim(ymin=90, ymax=100.5)
 # Label the Y axis to specify what the numbers mean
-plt.ylabel('False Positives')
+plt.ylabel('Recall')
 # Draw a legend for the two learning classes
 conventional_patch = mpatches.Patch(color='gray', label='Conventional Analysis')
 machine_learning_patch = mpatches.Patch(color='green', label='Machine Learning')
