c5c809b8d0195e00c35bfba952ace407b135d118
brendon-ai <brendon-m@outlook.com>
2018-12-05 10:22:52 -0500

    Re-adapted for topological CNN (load with JSON)

--- statistics/pulse_count_csv.py
+++ statistics/pulse_count_csv.py
@@ -3,34 +3,29 @@
 # Created by Brendon Matusch, December 2018
 
 import csv
+import json
 import sys
 
 import numpy as np
 
-# Read the log file from standard input, only taking the lines that include removal statistics
-lines = [line for line in sys.stdin.readlines() if 'removed' in line]
-# Read the numerical values into a NumPy array
-data = np.array([float(line.split()[-1]) for line in lines])
-# Reshape the array so the two values in the third dimension represents proportions of neck alphas and nuclear recoils removed, respectively
-data = np.reshape(data, (len(data) // 2, 2))
-# Convert it to a list so other data types can be added
-data = data.tolist()
+# Load performance data from standard input
+data = json.load(sys.stdin)
 
 # Create a list of tuples sof all the combinations in order
 hyperparameter_combinations = []
-for l2_lambda in [0, 0.0003, 0.0006, 0.001, 0.003]:
-    for dropout in [0, 0.25, 0.5]:
-        for hidden_layers in [2, 4, 6]:
-            for activation in ['relu', 'tanh']:
-                hyperparameter_combinations.append([l2_lambda, dropout, hidden_layers, activation])
-
-# print(len(data))
-# print(len(hyperparameter_combinations))
+for num_convolutional_layers in [3, 6]:
+    for l2_regularization in [0, 0.0005, 0.001, 0.003]:
+        for final_convolutional_layer_filters in [8]:
+            for kernel_radius_2_first_layer in [0]:
+                for nuclear_recoil_weight in [0.005, 0.01, 0.02]:
+                    for activation in ['relu', 'tanh']:
+                        hyperparameter_combinations.append([num_convolutional_layers, l2_regularization, final_convolutional_layer_filters,
+                                                            kernel_radius_2_first_layer, nuclear_recoil_weight, activation])
 
 # Create a CSV writer for standard output
 writer = csv.writer(sys.stdout)
 # Write the column titles
-writer.writerow(['L2 Lambda', 'Dropout', 'Hidden Layers', 'Activation', 'Neck Alphas Removed', 'Simulated WIMPs Removed'])
+writer.writerow(['Conv Layers', 'L2 Lambda', 'Conv Filters', 'Kernel Radius', 'Class Weighting', 'Activation', 'Neck Alphas Removed', 'Simulated WIMPs Removed'])
 # Iterate over hyperparameters with corresponding removal statistics
 for removal_stats, hyperparameters in zip(data, hyperparameter_combinations):
     # Append them together into one list, which will be a CSV line
