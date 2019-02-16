89916433ea3f690e7ee8f28c8b8b9f0f1f8820be
brendon-ai <brendon-m@outlook.com>
2019-02-15 21:29:23 -0500

    Added real world test data to graph

--- visualization/visualize_overall_performance_deap.py
+++ visualization/visualize_overall_performance_deap.py
@@ -9,9 +9,9 @@ import matplotlib.ticker as mtick
 
 # Create lists containing the performance statistics (in the form (mean, 8th percentile, 92nd percentile))
 ALPHA = [(0.996, None, None), (1.0, 1.0, 1.0), (0.9986762886158047, 0.996774193548387, 1.0),
-         (0.9985087719298246, 0.9921052631578948, 1.0), (0.9959030977982591, 0.9914285714285715, 1.0)]
+         (0.9985087719298246, 0.9921052631578948, 1.0), (0.9959030977982591, 0.9914285714285715, 1.0), (0.9120370370370369, 0.8888888888888888, 0.9333333333333333)]
 WIMP = [(0.91, None, None), (1.0, 1.0, 1.0), (0.7672341098533079, 0.74375, 0.79915611814346),
-        (0.9301859015688803, 0.6060606060606061, 1.0), (0.75745328295003, 0.7313432835820896, 0.7927631578947368)]
+        (0.9301859015688803, 0.6060606060606061, 1.0), (0.75745328295003, 0.7313432835820896, 0.7927631578947368), (0.7388888888888889, 0.7000000000000001, 0.775)]
 # Zip all of these lists into separate high and low arrays
 alpha_mean, alpha_low, alpha_high = np.array(list(zip(*ALPHA)))
 wimp_mean, wimp_low, wimp_high = np.array(list(zip(*WIMP)))
@@ -27,7 +27,7 @@ for array, mean in [(alpha_high, alpha_mean), (alpha_low, alpha_mean), (wimp_hig
             array[index] = np.abs(array[index] - mean[index])
 
 # The names of the configurations corresponding to all these lists
-CONFIGURATIONS = ['Conventional Clas.', 'Dense Neural Network', 'Map Projection CNN', 'Topological CNN', 'Map Projection CNN Test']
+CONFIGURATIONS = ['Conventional Clas.', 'Dense Neural Network', 'Map Projection CNN', 'Topological CNN', 'Map CNN Test (Sim)', 'Map CNN Test (Real)']
 
 # Get the locations for the bars
 BAR_WIDTH = 0.4
@@ -38,7 +38,7 @@ fig, ax = plt.subplots(figsize=(6, 3))
 # Use percentage labels for the Y axis
 ax.yaxis.set_major_formatter(mtick.PercentFormatter())
 # Draw a bar graph using the performance statistics
-plt.bar(alpha_locations, alpha_mean * 100, width=BAR_WIDTH, yerr=(alpha_low * 100, alpha_high * 100), label='Alpha Removal')
+plt.bar(alpha_locations, alpha_mean * 100, width=BAR_WIDTH, yerr=(alpha_low * 100, alpha_high * 100), label='Alpha/Background Removal')
 plt.bar(wimp_locations, wimp_mean * 100, width=BAR_WIDTH, yerr=(wimp_low * 100, wimp_high * 100), label='WIMP Removal')
 # Now set the configuration names, with words angled so they fit
 plt.xticks(wimp_locations, CONFIGURATIONS, rotation=15)
