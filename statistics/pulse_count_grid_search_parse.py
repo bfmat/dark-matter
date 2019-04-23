7db59a68749e1d201370cbb32204f74504ee1c0a
brendon-ai <brendon-m@outlook.com>
2019-04-23 14:47:15 -0400

    Converted parsing script to generate CSV

--- statistics/pulse_count_grid_search_parse.py
+++ statistics/pulse_count_grid_search_parse.py
@@ -6,6 +6,14 @@ import sys
 
 import numpy as np
 
+# Recalculate the list of hyperparameters to add to the results sheet
+hyperparameters = []
+for l2_lambda in [0, 0.0003, 0.0006, 0.001, 0.003]:
+    for dropout in [0, 0.25, 0.5]:
+        for hidden_layers in [2, 4, 6]:
+            hyperparameters.append([l2_lambda, dropout, hidden_layers])
+
+
 # Read the log file from standard input, only taking the lines that include removal statistics
 lines = [line for line in sys.stdin.readlines() if 'removed' in line]
 # If there are 1564 such lines (this is the incomplete first half of the map projection search) cut it to its proper length
@@ -22,18 +30,12 @@ statistics = np.concatenate([
     np.max(data, axis=1),
     np.min(data, axis=1)
 ], axis=1)
+# mean_alpha, mean_wimp, std_alpha, std_wimp, max_alpha, max_wimp, min_alpha, min_wimp = statistics[configuration_index]
+# Print an initial line for the CSV
+print('L2 lambda,Dropout,Hidden layers,Mean alpha removal,Mean WIMP removal,Alpha removal std dev,WIMP removal std dev,Max alpha removal,Max WIMP removal,Min alpha removal,Min WIMP removal')
 # Print out each of the good runs in a nicely formatted list
-for configuration_index, (mean_alpha, mean_wimp, std_alpha, std_wimp, max_alpha, max_wimp, min_alpha, min_wimp) in enumerate(statistics):
-    # Print only configurations where the mean neck alpha removal is over 99.6% (in line with the conventional discriminator)
-    if mean_alpha < 0.99:
-        continue
-    # Also filter for runs where less than 100% of WIMPs are removed (the network doesn't just output 1 for every example)
-    if mean_wimp == 1:
-        continue
-    # Now print out all of the relevant data
-    print('CONFIGURATION', configuration_index)
-    print('Mean alpha removal:', mean_alpha, 'Mean WIMP removal:', mean_wimp, 'Std dev of alpha removal:', std_alpha, 'Std dev of WIMP removal:', std_wimp)
-    print('Max alpha removal:', max_alpha, 'Max WIMP removal:', max_wimp, 'Min alpha removal:', min_alpha, 'Min WIMP removal:', min_wimp)
-    print('Neck alpha raw data:', sorted(data[configuration_index, :, 0].tolist()))
-    print('WIMP raw data:', sorted(data[configuration_index, :, 1].tolist()))
-    print()
+for configuration_index in range(len(statistics)):
+    # Combine the hyperparameter and result table rows
+    data_out = np.concatenate([hyperparameters[configuration_index], statistics[configuration_index]])
+    # Output the combined row of data in CSV format
+    print(*data_out, sep=',')
